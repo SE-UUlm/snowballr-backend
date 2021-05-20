649cf8190c75662697ae6a7907b82d08a4baaa6c
Lukas Romer <lukas.romer@uni-ulm.de>
2021-05-20 13:57:06 +0000

    feat: dev environment to test deno on host

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
 import { DataTypes, Database, Model } from 'https://deno.land/x/denodb/mod.ts';
 import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const PostgresDB = Deno.env.get("POSTGRES_DB");
-const PostgresUser = Deno.env.get("POSTGRES_USER");
-const PostgresPassword = Deno.env.get("POSTGRES_PASSWORD");
+var PostgresHost;
+var PostgresDB;
+var PostgresUser;
+var PostgresPassword;
 
-// await sleep(60)
+function getEnv() {
+  PostgresHost = Deno.env.get("POSTGRES_HOST");
+  PostgresDB = Deno.env.get("POSTGRES_DB");
+  PostgresUser = Deno.env.get("POSTGRES_USER");
+  PostgresPassword = Deno.env.get("POSTGRES_PASSWORD");
+}
+getEnv();
+
+if (PostgresHost == undefined) {
+  PostgresHost = "postgresdb"
+}
+if (PostgresDB == undefined || PostgresUser == undefined || PostgresPassword == undefined) {
+  console.log("Hello");
+  config({export: true});
+  console.log(Deno.env.get("POSTGRES_DB"));
+  getEnv();
+}
 
 const db = new Database('postgres', {
-  host: 'postgresdb',
+  host: PostgresHost,
   username: PostgresUser,
   password: PostgresPassword,
-  database: PostgresDB,
+  database: PostgresDB
 });
+
 
 class Flight extends Model {
   static table = 'flights';
