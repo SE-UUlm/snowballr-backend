a02e81e5912a001d8063785bc40e08aa24ab6f6b
andreas decker <andreas.decker@uni-ulm.de>
2021-06-16 12:55:16 +0200

    feat: logintoken in login response

--- src/controller/session.ts
+++ src/controller/session.ts
@@ -1,15 +1,13 @@
 import {createJWT} from "./validation.ts";
 import {insertToken} from "./databaseFetcher/token.ts";
 import {User} from "../model/db/user.ts";
-import {Context} from 'https://deno.land/x/oak/mod.ts';
 
 /**
  * starts a session by creating a JWT and inserting it into the database
  * @param user
- * @param ctx
  */
-export const startSession = async (user: User, ctx: Context) => {
+export const startSession = async (user: User) => {
     let jwt = await createJWT(user);
     await insertToken(user, jwt);
-    ctx.cookies.set("token", jwt, {httpOnly: true});
+    return jwt;
 }
