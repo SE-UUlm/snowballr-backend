cebcc28f548151ada3b38cb3e76f0d0e93d4ece3
andreas decker <andreas.decker@uni-ulm.de>
2021-06-17 16:44:48 +0200

    feat: add different port in mail if one is added in env different to 443

--- src/helper/url.ts
+++ src/helper/url.ts
@@ -4,6 +4,12 @@
  * @param url
  */
 export const urlSanitizer = (url: string) => {
+    let port = Number(Deno.env.get("PORT"));
+    if (port) {
+        if (port !== 443) {
+            url += port;
+        }
+    }
     if (url.startsWith("http://")) {
         url = url.replace("http://", "https://");
     }
