07b65a8ff76bb0086ba5c53c405b1e2ec20fe901
andreas decker <andreas.decker@uni-ulm.de>
2021-06-19 11:07:20 +0200

    feat: cookie now in header

--- src/controller/logout.ts
+++ src/controller/logout.ts
@@ -9,10 +9,9 @@ import {Context} from 'https://deno.land/x/oak/mod.ts';
  */
 export const logout = async (ctx: Context) => {
     let payload = await getPayloadFromJWT(ctx);
-    let token = await ctx.cookies.get("token");
+    let token = await ctx.request.headers.get("authenticationToken");
     if (token && payload) {
         await getToken(payload.id, token).then(async loginToken => loginToken ? loginToken.delete() : undefined);
     }
-    ctx.cookies.delete("token");
     ctx.response.status = 200;
 }
