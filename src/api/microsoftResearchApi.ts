667ee8b5672eae4ba02783013e4faa858b132eed
Lukas Romer <lukas.romer@uni-ulm.de>
2021-07-09 07:48:44 +0000

    formatting

--- src/api/microsoftResearchApi.ts
+++ src/api/microsoftResearchApi.ts
@@ -1,249 +1,249 @@
-import {IApiQuery} from "./iApiQuery.ts";
-import {IApiResponse} from "./iApiResponse.ts";
-import {IApiFetcher} from "./iApiFetcher.ts";
-import {IApiPaper, sourceApi} from './iApiPaper.ts';
-import {logger} from "./logger.ts";
-import {IApiAuthor} from "./iApiAuthor.ts";
-import {IApiUniqueId, idType} from "./iApiUniqueId.ts";
+import { IApiQuery } from "./iApiQuery.ts";
+import { IApiResponse } from "./iApiResponse.ts";
+import { IApiFetcher } from "./iApiFetcher.ts";
+import { IApiPaper, sourceApi } from './iApiPaper.ts';
+import { logger } from "./logger.ts";
+import { IApiAuthor } from "./iApiAuthor.ts";
+import { IApiUniqueId, idType } from "./iApiUniqueId.ts";
 
 export class MicrosoftResearchApi implements IApiFetcher {
-    url: string;
-    private _authToken: string;
-    private _headers: {};
-    private _attributes: string;
-    private _paperTypeMapper: string[];
-    private _queryAttributeMapper: {};
+	url: string;
+	private _authToken: string;
+	private _headers: {};
+	private _attributes: string;
+	private _paperTypeMapper: string[];
+	private _queryAttributeMapper: {};
 
-    public constructor(url: string, authToken: string) {
-        logger.info("MicrosoftResearchApi initialized");
-        this.url = url;
-        this._authToken = authToken;
-        this._headers = {"Ocp-Apim-Subscription-Key": this._authToken, "Content-Type": "application/json"};
-        this._attributes = "Id,IA,RId,CC,Y,PB,Pt,S,Ti,AA.AuN,DOI";
-        this._paperTypeMapper = ["Unknown", "Journal article", "Patent", "Conference paper", "Book chapter", "Book", "Book reference entry", "Dataset", "Repository"];
-        this._queryAttributeMapper = {
-            "author": "AA.AuN",
-            "title": "Ti",
-            "id": "DOI",
-            "year": "Y",
-            "publisher": "PB",
-            "type": "Pt"
-        };
-    }
+	public constructor(url: string, authToken: string) {
+		logger.info("MicrosoftResearchApi initialized");
+		this.url = url;
+		this._authToken = authToken;
+		this._headers = { "Ocp-Apim-Subscription-Key": this._authToken, "Content-Type": "application/json" };
+		this._attributes = "Id,IA,RId,CC,Y,PB,Pt,S,Ti,AA.AuN,DOI";
+		this._paperTypeMapper = ["Unknown", "Journal article", "Patent", "Conference paper", "Book chapter", "Book", "Book reference entry", "Dataset", "Repository"];
+		this._queryAttributeMapper = {
+			"author": "AA.AuN",
+			"title": "Ti",
+			"id": "DOI",
+			"year": "Y",
+			"publisher": "PB",
+			"type": "Pt"
+		};
+	}
 
-    /**
-     * Abstract is provided via dictionary. This function casts an IA-object to a single string.
-     *
-     * @param iA - invertedAbstract object provided by microsoft api
-     * @returns abstract of a paper in a single string.
-     */
-    private static _convertInvertedAbstract(iA: any): string[] {
-        let abstr: string = "";
-        try {
-            for (let key in iA.InvertedIndex) {
-                abstr += " " + key;
-            }
-            return [abstr.trim()];
-        } catch (error) {
-            logger.error("Couldn't convert InvertedAbstract from Microsoft API to a single string: " + error);
-            //logger.critical(iA);
-            return [];
-        }
-    }
+	/**
+	 * Abstract is provided via dictionary. This function casts an IA-object to a single string.
+	 *
+	 * @param iA - invertedAbstract object provided by microsoft api
+	 * @returns abstract of a paper in a single string.
+	 */
+	private static _convertInvertedAbstract(iA: any): string[] {
+		let abstr: string = "";
+		try {
+			for (let key in iA.InvertedIndex) {
+				abstr += " " + key;
+			}
+			return [abstr.trim()];
+		} catch (error) {
+			logger.error("Couldn't convert InvertedAbstract from Microsoft API to a single string: " + error);
+			//logger.critical(iA);
+			return [];
+		}
+	}
 
-    /**
-     * Checks for a paper at the microsoft research api via a query object.
-     * Returns the papers gathered info and all papers citated or referenced.
-     *
-     * @param query - Object defined by interface in IApiQuery to filter and query api calls.
-     * @returns Object containing the fetched paper and all paperObjects from citations and references. Promise.
-     */
-    public async fetch(query: IApiQuery): Promise<IApiResponse> {
-        var citations: IApiPaper[];
-        var paper: IApiPaper;
-        var RIds: number[];
-        var references: IApiPaper[];
+	/**
+	 * Checks for a paper at the microsoft research api via a query object.
+	 * Returns the papers gathered info and all papers citated or referenced.
+	 *
+	 * @param query - Object defined by interface in IApiQuery to filter and query api calls.
+	 * @returns Object containing the fetched paper and all paperObjects from citations and references. Promise.
+	 */
+	public async fetch(query: IApiQuery): Promise<IApiResponse> {
+		var citations: IApiPaper[];
+		var paper: IApiPaper;
+		var RIds: number[];
+		var references: IApiPaper[];
 
-        let response = fetch(this.url, {
-            method: 'POST',
-            headers: this._headers,
-            body: JSON.stringify({
-                expr: this._parseQuery(query),
-                attributes: this._attributes
-            })
-        })
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                //logger.debug(data);
-                paper = this._parseResponse(data.entities[0]);
-                RIds = data.entities[0] ? data.entities[0].RId : [];
-                return data.entities[0] ? this._getCitations(data.entities[0].Id) : [];
-            })
-            .then(data => {
-                citations = data
-                return RIds.length > 0 ? this._getReferences(RIds) : [];
-            })
-            .then(data => {
-                references = data;
-                let apiReturn: IApiResponse = {
-                    "paper": paper,
-                    "citations": citations,
-                    "references": references
-                }
-                return apiReturn;
-            })
-            .catch(data => {
-                logger.error("Error while fetching microsoftResearch: " + data);
-                return {
-                    "paper": paper ? paper : undefined,
-                    "citations": citations ? citations : undefined,
-                    "references": references ? references : undefined
-                } as IApiResponse;
-            })
-        return response;
-    }
+		let response = fetch(this.url, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				expr: this._parseQuery(query),
+				attributes: this._attributes
+			})
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				//logger.debug(data);
+				paper = this._parseResponse(data.entities[0]);
+				RIds = data.entities[0] ? data.entities[0].RId : [];
+				return data.entities[0] ? this._getCitations(data.entities[0].Id) : [];
+			})
+			.then(data => {
+				citations = data
+				return RIds.length > 0 ? this._getReferences(RIds) : [];
+			})
+			.then(data => {
+				references = data;
+				let apiReturn: IApiResponse = {
+					"paper": paper,
+					"citations": citations,
+					"references": references
+				}
+				return apiReturn;
+			})
+			.catch(data => {
+				logger.error("Error while fetching microsoftResearch: " + data);
+				return {
+					"paper": paper ? paper : undefined,
+					"citations": citations ? citations : undefined,
+					"references": references ? references : undefined
+				} as IApiResponse;
+			})
+		return response;
+	}
 
-    /**
-     * Parse all microsoft-id references from a single paper and query for all the ids to return a paperObject for each.
-     *
-     * @param microsoftIds - list of microsoft-ids provided by the source-paper in key RId
-     * @returns list of paperObjects containing the references. Promise.
-     */
-    private _getReferences(microsoftIds: number[]): Promise<IApiPaper[]> {
-        let convertedIds: string[] = microsoftIds.map(String);
-        convertedIds = convertedIds.map(i => 'Id=' + i);
-        let queryPattern = convertedIds.join(',');
+	/**
+	 * Parse all microsoft-id references from a single paper and query for all the ids to return a paperObject for each.
+	 *
+	 * @param microsoftIds - list of microsoft-ids provided by the source-paper in key RId
+	 * @returns list of paperObjects containing the references. Promise.
+	 */
+	private _getReferences(microsoftIds: number[]): Promise<IApiPaper[]> {
+		let convertedIds: string[] = microsoftIds.map(String);
+		convertedIds = convertedIds.map(i => 'Id=' + i);
+		let queryPattern = convertedIds.join(',');
 
-        let response = fetch(this.url, {
-            method: 'POST',
-            headers: this._headers,
-            body: JSON.stringify({
-                expr: `Or(${queryPattern})`,
-                count: convertedIds.length,
-                attributes: this._attributes
-            })
-        })
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                let citations: Array<IApiPaper> = [];
-                for (let value in data.entities) {
-                    let cit = this._parseResponse(data.entities[value]);
-                    citations.push(cit);
-                }
-                return citations;
-            })
-        return response;
-    }
+		let response = fetch(this.url, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				expr: `Or(${queryPattern})`,
+				count: convertedIds.length,
+				attributes: this._attributes
+			})
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				let citations: Array<IApiPaper> = [];
+				for (let value in data.entities) {
+					let cit = this._parseResponse(data.entities[value]);
+					citations.push(cit);
+				}
+				return citations;
+			})
+		return response;
+	}
 
-    /**
-     * Query for all papers containing the original-papers microsoft id in their references
-     *
-     * @param microsoftId - original-paper microsoft id. Returned by another fetch call.
-     * @returns list of paperObjects containing the citations. Promise.
-     */
-    private _getCitations(microsoftId: string): Promise<IApiPaper[]> {
-        let response = fetch(this.url, {
-            method: 'POST',
-            headers: this._headers,
-            body: JSON.stringify({
-                expr: `RId=${microsoftId}`,
-                count: 1000,
-                attributes: this._attributes
-            })
-        })
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                var citations: Array<IApiPaper> = [];
-                for (let value in data.entities) {
-                    let cit = this._parseResponse(data.entities[value]);
-                    citations.push(cit);
-                }
-                return citations;
-            })
-            .then(
-                (result) => {
-                    return result; // This returns undefined
-                },
-                (error) => {
-                    return error;
-                }
-            );
-        return response;
-    }
+	/**
+	 * Query for all papers containing the original-papers microsoft id in their references
+	 *
+	 * @param microsoftId - original-paper microsoft id. Returned by another fetch call.
+	 * @returns list of paperObjects containing the citations. Promise.
+	 */
+	private _getCitations(microsoftId: string): Promise<IApiPaper[]> {
+		let response = fetch(this.url, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				expr: `RId=${microsoftId}`,
+				count: 1000,
+				attributes: this._attributes
+			})
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				var citations: Array<IApiPaper> = [];
+				for (let value in data.entities) {
+					let cit = this._parseResponse(data.entities[value]);
+					citations.push(cit);
+				}
+				return citations;
+			})
+			.then(
+				(result) => {
+					return result; // This returns undefined
+				},
+				(error) => {
+					return error;
+				}
+			);
+		return response;
+	}
 
-    /**
-     * Parse a queryObject to a string readable by the microsoftApi
-     *
-     * @param query - queryObject for the originalPaper.
-     * @returns string appendable to the api call via body-key "expr".
-     */
-    private _parseQuery(query: IApiQuery): string {
-        let baseExpr: string = `Or(DOI='${query.id}', And(Composite(AA.AuN='${query.rawName.toLowerCase()}'), Ti='${query.title.toLowerCase()}'))`
+	/**
+	 * Parse a queryObject to a string readable by the microsoftApi
+	 *
+	 * @param query - queryObject for the originalPaper.
+	 * @returns string appendable to the api call via body-key "expr".
+	 */
+	private _parseQuery(query: IApiQuery): string {
+		let baseExpr: string = `Or(DOI='${query.id}', And(Composite(AA.AuN='${query.rawName.toLowerCase()}'), Ti='${query.title.toLowerCase()}'))`
 
-        return baseExpr;
-    }
+		return baseExpr;
+	}
 
-    /**
-     * Cast the response of a single paper return by the microsoftApi to a ApiPaper object
-     * Used to get normalized result of all apis.
-     *
-     * @param response - single object return for a single paper by the microsoftApi
-     * @returns normalized ApiPaper object.
-     */
-    private _parseResponse(response: any): IApiPaper {
-        //logger.debug(response);
-        let refLength: number = (response.RId === undefined ? 0 : response.RId.length);
+	/**
+	 * Cast the response of a single paper return by the microsoftApi to a ApiPaper object
+	 * Used to get normalized result of all apis.
+	 *
+	 * @param response - single object return for a single paper by the microsoftApi
+	 * @returns normalized ApiPaper object.
+	 */
+	private _parseResponse(response: any): IApiPaper {
+		//logger.debug(response);
+		let refLength: number = (response.RId === undefined ? 0 : response.RId.length);
 
-        let parsedAuthors: IApiAuthor[] = [];
-        for (let a of response.AA) {
-            let parsedAuthor: IApiAuthor = {
-                id: undefined,
-                orcid: undefined,
-                rawString: a.AuN,
-                lastName: undefined,
-                firstName: undefined,
-            }
-            parsedAuthors.push(parsedAuthor);
-        }
+		let parsedAuthors: IApiAuthor[] = [];
+		for (let a of response.AA) {
+			let parsedAuthor: IApiAuthor = {
+				id: undefined,
+				orcid: undefined,
+				rawString: a.AuN,
+				lastName: undefined,
+				firstName: undefined,
+			}
+			parsedAuthors.push(parsedAuthor);
+		}
 
-        let parsedUniqueIds: IApiUniqueId[] = [];
-        parsedUniqueIds.push(
-            {
-                id: undefined,
-                type: idType.DOI,
-                value: response.DOI ? response.DOI : undefined,
-            } as IApiUniqueId
-        )
-        parsedUniqueIds.push(
-            {
-                id: undefined,
-                type: idType.MicrosoftAcademic,
-                value: response.Id ? response.Id : undefined,
-            } as IApiUniqueId
-        )
+		let parsedUniqueIds: IApiUniqueId[] = [];
+		parsedUniqueIds.push(
+			{
+				id: undefined,
+				type: idType.DOI,
+				value: response.DOI ? response.DOI : undefined,
+			} as IApiUniqueId
+		)
+		parsedUniqueIds.push(
+			{
+				id: undefined,
+				type: idType.MicrosoftAcademic,
+				value: response.Id ? response.Id : undefined,
+			} as IApiUniqueId
+		)
 
-        let parsedResponse: IApiPaper = {
-            id: undefined,
-            title: response.Ti ? [response.Ti] : [],
-            author: parsedAuthors,
-            abstract: response.IA ? MicrosoftResearchApi._convertInvertedAbstract(response.IA) : [],
-            numberOfReferences: refLength ? [refLength] : [],
-            numberOfCitations: response.CC ? [response.CC] : [],
-            year: response.Y ? Number(response.Y) : undefined,
-            publisher: response.PB ? [response.PB] : [],
-            type: response.Pt ? this._paperTypeMapper[response.Pt] : undefined,
-            scope: undefined,
-            scopeName: undefined,
-            pdf: response.S ? response.S.map((item: any) => item.U) : undefined,
-            uniqueId: parsedUniqueIds,
-            source: [sourceApi.MA]
-        };
-        return parsedResponse;
-    }
+		let parsedResponse: IApiPaper = {
+			id: undefined,
+			title: response.Ti ? [response.Ti] : [],
+			author: parsedAuthors,
+			abstract: response.IA ? MicrosoftResearchApi._convertInvertedAbstract(response.IA) : [],
+			numberOfReferences: refLength ? [refLength] : [],
+			numberOfCitations: response.CC ? [response.CC] : [],
+			year: response.Y ? Number(response.Y) : undefined,
+			publisher: response.PB ? [response.PB] : [],
+			type: response.Pt ? this._paperTypeMapper[response.Pt] : undefined,
+			scope: undefined,
+			scopeName: undefined,
+			pdf: response.S ? response.S.map((item: any) => item.U) : undefined,
+			uniqueId: parsedUniqueIds,
+			source: [sourceApi.MA]
+		};
+		return parsedResponse;
+	}
 }
--- src/api/semanticScholar.ts
+++ src/api/semanticScholar.ts
@@ -1,130 +1,130 @@
-import {IApiQuery} from "./iApiQuery.ts";
-import {IApiResponse} from "./iApiResponse.ts";
-import {IApiFetcher} from "./iApiFetcher.ts";
-import {IApiPaper, sourceApi} from './iApiPaper.ts';
-import {logger} from "./logger.ts";
-import {IApiAuthor} from "./iApiAuthor.ts";
-import {IApiUniqueId, idType} from "./iApiUniqueId.ts";
+import { IApiQuery } from "./iApiQuery.ts";
+import { IApiResponse } from "./iApiResponse.ts";
+import { IApiFetcher } from "./iApiFetcher.ts";
+import { IApiPaper, sourceApi } from './iApiPaper.ts';
+import { logger } from "./logger.ts";
+import { IApiAuthor } from "./iApiAuthor.ts";
+import { IApiUniqueId, idType } from "./iApiUniqueId.ts";
 
 export class SemanticScholar implements IApiFetcher {
-    url: string;
+	url: string;
 
-    public constructor(url: string) {
-        logger.info("SemanticScholar initialized");
-        this.url = url;
-    }
+	public constructor(url: string) {
+		logger.info("SemanticScholar initialized");
+		this.url = url;
+	}
 
-    /**
-     * Checks for a paper at the microsoft research api via a query object.
-     * Returns the papers gathered info and all papers citated or referenced.
-     *
-     * @param query - Object defined by interface in IApiQuery to filter and query api calls.
-     * @returns Object containing the fetched paper and all paperObjects from citations and references. Promise.
-     */
-    public async fetch(query: IApiQuery): Promise<IApiResponse> {
-        var citations: IApiPaper[];
-        var paper: IApiPaper;
-        var RIds: number[];
-        var references: IApiPaper[];
+	/**
+	 * Checks for a paper at the microsoft research api via a query object.
+	 * Returns the papers gathered info and all papers citated or referenced.
+	 *
+	 * @param query - Object defined by interface in IApiQuery to filter and query api calls.
+	 * @returns Object containing the fetched paper and all paperObjects from citations and references. Promise.
+	 */
+	public async fetch(query: IApiQuery): Promise<IApiResponse> {
+		var citations: IApiPaper[];
+		var paper: IApiPaper;
+		var RIds: number[];
+		var references: IApiPaper[];
 
-        let response = fetch(`${this.url}/${query.id}`)
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                //logger.debug(data);
-                paper = this._parseResponse(data);
-                citations = this._getChildren(data.citations);
-                references = this._getChildren(data.references);
-            })
-            .then(data => {
-                let apiReturn: IApiResponse = {
-                    "paper": paper,
-                    "citations": citations,
-                    "references": references
-                }
-                return apiReturn;
-            })
-            .catch(data => {
-                logger.error("Error while fetching semanticScholar: " + data);
-                return {
-                    "paper": paper ? paper : undefined,
-                    "citations": citations ? citations : undefined,
-                    "references": references ? references : undefined
-                } as IApiResponse;
-            })
-        return response;
-    }
+		let response = fetch(`${this.url}/${query.id}`)
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				//logger.debug(data);
+				paper = this._parseResponse(data);
+				citations = this._getChildren(data.citations);
+				references = this._getChildren(data.references);
+			})
+			.then(data => {
+				let apiReturn: IApiResponse = {
+					"paper": paper,
+					"citations": citations,
+					"references": references
+				}
+				return apiReturn;
+			})
+			.catch(data => {
+				logger.error("Error while fetching semanticScholar: " + data);
+				return {
+					"paper": paper ? paper : undefined,
+					"citations": citations ? citations : undefined,
+					"references": references ? references : undefined
+				} as IApiResponse;
+			})
+		return response;
+	}
 
-    /**
-     * Parse all microsoft-id references from a single paper and query for all the ids to return a paperObject for each.
-     *
-     * @param microsoftIds - list of microsoft-ids provided by the source-paper in key RId
-     * @returns list of paperObjects containing the references. Promise.
-     */
-    private _getChildren(children: []): IApiPaper[] {
-        let parsedChildren: IApiPaper[] = [];
-        for (let child in children) {
-            let parsedChild = this._parseResponse(children[child]);
-            parsedChildren.push(parsedChild);
-        }
-        return parsedChildren;
-    }
+	/**
+	 * Parse all microsoft-id references from a single paper and query for all the ids to return a paperObject for each.
+	 *
+	 * @param microsoftIds - list of microsoft-ids provided by the source-paper in key RId
+	 * @returns list of paperObjects containing the references. Promise.
+	 */
+	private _getChildren(children: []): IApiPaper[] {
+		let parsedChildren: IApiPaper[] = [];
+		for (let child in children) {
+			let parsedChild = this._parseResponse(children[child]);
+			parsedChildren.push(parsedChild);
+		}
+		return parsedChildren;
+	}
 
-    /**
-     * Cast the response of a single paper return by the microsoftApi to a ApiPaper object
-     * Used to get normalized result of all apis.
-     *
-     * @param response - single object return for a single paper by the microsoftApi
-     * @returns normalized ApiPaper object.
-     */
-    private _parseResponse(response: any): IApiPaper {
-        //logger.debug(response);
+	/**
+	 * Cast the response of a single paper return by the semanticScholar to a ApiPaper object
+	 * Used to get normalized result of all apis.
+	 *
+	 * @param response - single object return for a single paper by the semanticScholar
+	 * @returns normalized ApiPaper object.
+	 */
+	private _parseResponse(response: any): IApiPaper {
+		//logger.debug(response);
 
-        let parsedAuthors: IApiAuthor[] = [];
-        for (let a of response.authors) {
-            let parsedAuthor: IApiAuthor = {
-                id: undefined,
-                orcid: undefined,
-                rawString: a.name,
-                lastName: undefined,
-                firstName: undefined,
-            }
-            parsedAuthors.push(parsedAuthor);
-        }
+		let parsedAuthors: IApiAuthor[] = [];
+		for (let a of response.authors) {
+			let parsedAuthor: IApiAuthor = {
+				id: undefined,
+				orcid: undefined,
+				rawString: a.name,
+				lastName: undefined,
+				firstName: undefined,
+			}
+			parsedAuthors.push(parsedAuthor);
+		}
 
-        let parsedUniqueIds: IApiUniqueId[] = [];
-        parsedUniqueIds.push(
-            {
-                id: undefined,
-                type: idType.DOI,
-                value: response.doi ? response.doi : undefined,
-            } as IApiUniqueId
-        )
-        parsedUniqueIds.push(
-            {
-                id: undefined,
-                type: idType.SemanticScholar,
-                value: response.paperId ? response.paperId : undefined,
-            } as IApiUniqueId
-        )
+		let parsedUniqueIds: IApiUniqueId[] = [];
+		parsedUniqueIds.push(
+			{
+				id: undefined,
+				type: idType.DOI,
+				value: response.doi ? response.doi : undefined,
+			} as IApiUniqueId
+		)
+		parsedUniqueIds.push(
+			{
+				id: undefined,
+				type: idType.SemanticScholar,
+				value: response.paperId ? response.paperId : undefined,
+			} as IApiUniqueId
+		)
 
-        let parsedResponse: IApiPaper = {
-            id: undefined,
-            title: response.title ? [response.title] : [],
-            author: parsedAuthors,
-            abstract: response.abstract ? [response.abstract] : [],
-            numberOfReferences: response.references ? [response.references.length] : [],
-            numberOfCitations: response.citations ? [response.citations.length] : [],
-            year: response.year ? Number(response.year) : undefined,
-            publisher: undefined,
-            type: undefined,
-            scope: undefined,
-            scopeName: response.venue ? [response.venue] : undefined,
-            pdf: response.url ? [response.url] : undefined,
-            uniqueId: parsedUniqueIds,
-            source: [sourceApi.S2]
-        };
-        return parsedResponse;
-    }
+		let parsedResponse: IApiPaper = {
+			id: undefined,
+			title: response.title ? [response.title] : [],
+			author: parsedAuthors,
+			abstract: response.abstract ? [response.abstract] : [],
+			numberOfReferences: response.references ? [response.references.length] : [],
+			numberOfCitations: response.citations ? [response.citations.length] : [],
+			year: response.year ? Number(response.year) : undefined,
+			publisher: undefined,
+			type: undefined,
+			scope: undefined,
+			scopeName: response.venue ? [response.venue] : undefined,
+			pdf: response.url ? [response.url] : undefined,
+			uniqueId: parsedUniqueIds,
+			source: [sourceApi.S2]
+		};
+		return parsedResponse;
+	}
 }
