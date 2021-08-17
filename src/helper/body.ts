e267edc59878d010f9cfc99f7dc8e65a34973b17
Andreas Decker <andreas.decker@uni-ulm.de>
2021-08-17 21:07:06 +0200

    fix: incompatible versions with deno 11.5

--- src/helper/body.ts
+++ src/helper/body.ts
@@ -1,6 +1,6 @@
-import {Context} from 'https://deno.land/x/oak/mod.ts';
+import { Context } from "https://deno.land/x/oak@v8.0.0/mod.ts";
 import { makePromise } from "./assign.ts";
-import {makeErrorMessage} from "./error.ts";
+import { makeErrorMessage } from "./error.ts";
 
 /**
  * Converts the Body to a json object, if possible.
@@ -9,7 +9,7 @@ import {makeErrorMessage} from "./error.ts";
  */
 export const jsonBodyToObject = (ctx: Context) => {
     try {
-        return ctx.request.body({type: "json"}).value;
+        return ctx.request.body({ type: "json" }).value;
     } catch (err) {
         makeErrorMessage(ctx, 422, "noBodyProvided")
     }
