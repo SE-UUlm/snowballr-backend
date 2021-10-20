bb2544d9507f7f08c2abd7c61bfc707efcbd16c3
andreas decker <andreas.decker@uni-ulm.de>
2021-10-20 14:03:10 +0200

    json stringify

--- src/controller/project.controller.ts
+++ src/controller/project.controller.ts
@@ -1140,6 +1140,8 @@ const startFetchFromProjectPaper = async (ppID: number, stageID: number, project
 const getFinalDecisionOfPaper = (reviews: ReviewMessage[], project: Project, ppID: number, lowerTreshold: number, upperTreshold: number) => {
     let allReviewsFinished = !reviews.some(review => !review.finished)
     console.log("finished reviews: " + allReviewsFinished)
+    console.log("project: " + JSON.stringify(project))
+    console.log("minCount" + project.minCountReviewers)
     console.log(`${reviews.length >= Number(project.minCountReviewers)} ${reviews.length} ${Number(project.minCountReviewers)}`)
     if (allReviewsFinished && reviews.length >= Number(project.minCountReviewers)) {
         let decisionNumber = 0;
