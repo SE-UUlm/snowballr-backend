c41731d0f9558e33277b76610006d0c6f875357f
Lukas Romer <lukas.romer@uni-ulm.de>
2022-03-24 16:03:32 +0100

    m

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -26,28 +26,28 @@ const URL = Deno.env.get("URL");
  * @param client Only necessary to throw in an empty mock to not send mails during tests
  */
 export const createUser = async (ctx: Context, client: EMailClient) => {
-	let validate = await validateUserEntry(ctx, [], UserStatus.needsPO, -1, { needed: true, params: ["email"] })
-	if (validate) {
-		const payloadJson = await getPayloadFromJWTHeader(ctx);
+    let validate = await validateUserEntry(ctx, [], UserStatus.needsPO, -1, { needed: true, params: ["email"] })
+    if (validate) {
+        const payloadJson = await getPayloadFromJWTHeader(ctx);
 
-		try {
-			let user = await insertUserForRegistration(validate.email);
-			let jwt = await createRefreshJWT(Number(user.id))
-			await insertInvitation(user, jwt);
-			let linkText = "snowballR"
+        try {
+            let user = await insertUserForRegistration(validate.email);
+            let jwt = await createRefreshJWT(Number(user.id))
+            await insertInvitation(user, jwt);
+            let linkText = "snowballR"
 
-			if (adminMail) {
-				await sendInvitationMail(jwt, linkText, validate.email, Number(user.id), client, await getUserName(payloadJson));
-				ctx.response.status = 201;
-				ctx.response.body = JSON.stringify(convertUserToUserProfile(user))
-			} else {
-				console.error("no email in env!")
-				makeErrorMessage(ctx, 401, "not authorized")
-			}
-		} catch (err) {
-			makeErrorMessage(ctx, 422, "email already exists")
-		}
-	}
+            if (adminMail) {
+                await sendInvitationMail(jwt, linkText, validate.email, Number(user.id), client, await getUserName(payloadJson));
+                ctx.response.status = 201;
+                ctx.response.body = JSON.stringify(convertUserToUserProfile(user))
+            } else {
+                console.error("no email in env!")
+                makeErrorMessage(ctx, 401, "not authorized")
+            }
+        } catch (err) {
+            makeErrorMessage(ctx, 422, "email already exists")
+        }
+    }
 }
 
 /**
@@ -56,19 +56,19 @@ export const createUser = async (ctx: Context, client: EMailClient) => {
  * @param client
  */
 export const resetPassword = async (ctx: Context, client: EMailClient) => {
-	let validate = await validateUserEntry(ctx, [], UserStatus.none, -1, { needed: true, params: ["email"] })
-	if (validate) {
-		let user = await returnUserByEmail(validate.email)
-		if (user && URL) {
-			let jwt = await createJWT(user)
-			await insertResetToken(user, jwt);
-			let linkText = "snowballR"
-			await sendResetMail(jwt, linkText, validate.email, Number(user.id), client);
-			ctx.response.status = 200;
-		} else {
-			makeErrorMessage(ctx, 400, "wrong email provided")
-		}
-	}
+    let validate = await validateUserEntry(ctx, [], UserStatus.none, -1, { needed: true, params: ["email"] })
+    if (validate) {
+        let user = await returnUserByEmail(validate.email)
+        if (user && URL) {
+            let jwt = await createJWT(user)
+            await insertResetToken(user, jwt);
+            let linkText = "snowballR"
+            await sendResetMail(jwt, linkText, validate.email, Number(user.id), client);
+            ctx.response.status = 200;
+        } else {
+            makeErrorMessage(ctx, 400, "wrong email provided")
+        }
+    }
 }
 
 /**
@@ -76,14 +76,14 @@ export const resetPassword = async (ctx: Context, client: EMailClient) => {
  * @param ctx
  */
 export const getUsers = async (ctx: Context) => {
-	let validate = await validateUserEntry(ctx, [], UserStatus.needsPO, -1, { needed: false, params: [] })
-	if (validate) {
-		let users = await User.all();
-		let userProfile = users.map(user => convertUserToUserProfile(user));
-		let userMessage: UsersMessage = { users: userProfile }
-		ctx.response.body = JSON.stringify(userMessage)
-		ctx.response.status = 200;
-	}
+    let validate = await validateUserEntry(ctx, [], UserStatus.needsPO, -1, { needed: false, params: [] })
+    if (validate) {
+        let users = await User.all();
+        let userProfile = users.map(user => convertUserToUserProfile(user));
+        let userMessage: UsersMessage = { users: userProfile }
+        ctx.response.body = JSON.stringify(userMessage)
+        ctx.response.status = 200;
+    }
 }
 
 
@@ -93,17 +93,17 @@ export const getUsers = async (ctx: Context) => {
  * @param id
  */
 export const getUser = async (ctx: Context, id: number) => {
-	let validate = await validateUserEntry(ctx, [id], UserStatus.needsSameUserOrPO, -1, { needed: false, params: [] }, id)
-	if (validate) {
-		let user = await User.find(id);
-		if (user) {
-			let userProfile = convertUserToUserProfile(user);
-			ctx.response.body = JSON.stringify(userProfile);
-			ctx.response.status = 200;
-		} else {
-			makeErrorMessage(ctx, 404, "User not Found")
-		}
-	}
+    let validate = await validateUserEntry(ctx, [id], UserStatus.needsSameUserOrPO, -1, { needed: false, params: [] }, id)
+    if (validate) {
+        let user = await User.find(id);
+        if (user) {
+            let userProfile = convertUserToUserProfile(user);
+            ctx.response.body = JSON.stringify(userProfile);
+            ctx.response.status = 200;
+        } else {
+            makeErrorMessage(ctx, 404, "User not Found")
+        }
+    }
 }
 
 /**
@@ -112,14 +112,14 @@ export const getUser = async (ctx: Context, id: number) => {
  * @param id
  */
 export const getUserProjects = async (ctx: Context, id: number) => {
-	let validate = await validateUserEntry(ctx, [id], UserStatus.needsSameUser, -1, { needed: false, params: [] }, id)
-	if (validate) {
-		let userProjects = await getAllProjectsByUser(id)
-		if (userProjects) {
-			ctx.response.body = JSON.stringify(await convertProjectToProjectMessage(userProjects))
-			ctx.response.status = 200;
-		}
-	}
+    let validate = await validateUserEntry(ctx, [id], UserStatus.needsSameUser, -1, { needed: false, params: [] }, id)
+    if (validate) {
+        let userProjects = await getAllProjectsByUser(id)
+        if (userProjects) {
+            ctx.response.body = JSON.stringify(await convertProjectToProjectMessage(userProjects))
+            ctx.response.status = 200;
+        }
+    }
 }
 
 /**
@@ -129,70 +129,70 @@ export const getUserProjects = async (ctx: Context, id: number) => {
  * @param id
  */
 export const patchUser = async (ctx: Context, id: number | undefined) => {
-	if (!id) {
-		makeErrorMessage(ctx, 422, "no user id included")
-		return;
-	}
-	const payloadJson = await getPayloadFromJWTHeader(ctx);
-	let userData = await convertCtxBodyToUser(ctx);
-	let isAdmin = await checkAdmin(payloadJson);
-	let isPO = await checkPO(payloadJson);
+    if (!id) {
+        makeErrorMessage(ctx, 422, "no user id included")
+        return;
+    }
+    const payloadJson = await getPayloadFromJWTHeader(ctx);
+    let userData = await convertCtxBodyToUser(ctx);
+    let isAdmin = await checkAdmin(payloadJson);
+    let isPO = await checkPO(payloadJson);
 
-	let checkedToken = await checkToken(id, ctx, userData);
-	let isSameUser = (await getUserID(payloadJson)) === id || checkedToken.valid
-	let register = checkedToken.kind === "invitation"
-	if (isSameUser || isAdmin || isPO) {
+    let checkedToken = await checkToken(id, ctx, userData);
+    let isSameUser = (await getUserID(payloadJson)) === id || checkedToken.valid
+    let register = checkedToken.kind === "invitation"
+    if (isSameUser || isAdmin || isPO) {
 
-		let user = await changeUserData(ctx, id, isSameUser, isAdmin, userData, register)
-		if (user) {
-			let userProfile = convertUserToUserProfile(user);
-			ctx.response.body = JSON.stringify(userProfile);
-			ctx.response.status = 200;
-		}
-	} else {
+        let user = await changeUserData(ctx, id, isSameUser, isAdmin, userData, register)
+        if (user) {
+            let userProfile = convertUserToUserProfile(user);
+            ctx.response.body = JSON.stringify(userProfile);
+            ctx.response.status = 200;
+        }
+    } else {
 
-		if (ctx.response.status !== 400) {
-			makeErrorMessage(ctx, 401, "not authorized");
-		}
-	}
+        if (ctx.response.status !== 400) {
+            makeErrorMessage(ctx, 401, "not authorized");
+        }
+    }
 }
 
 const changeUserData = async (ctx: Context, id: number, isSameUser: boolean, isAdmin: boolean, userData: UserParameters, register: boolean) => {
-	let user = await User.find(id);
+    let user = await User.find(id);
 
-	if (!user) {
-		makeErrorMessage(ctx, 404, "User not Found")
-		return;
-	}
+    if (!user) {
+        makeErrorMessage(ctx, 404, "User not Found")
+        return;
+    }
 
-	if (isSameUser) {
-		if (userData.password) {
-			user.password = hashPassword(userData.password)
-		}
-	}
+    if (isSameUser) {
+        if (userData.password) {
+            user.password = hashPassword(userData.password)
+        }
+    }
 
-	if (register) {
-		user.status = "active"
-	}
-	if (isAdmin) {
-		if (userData.status) {
-			user.status = userData.status;
-		}
-		if (userData.isAdmin !== undefined) {
-			user.isAdmin = userData.isAdmin;
-		}
+    if (register) {
+        user.status = "active"
+    }
+    if (isAdmin) {
+        if (userData.status) {
+            user.status = userData.status;
+        }
+        if (userData.isAdmin !== undefined) {
+            user.isAdmin = userData.isAdmin;
+        }
 
-	}
-	if (userData.email) {
-		user.eMail = userData.email
-	}
-	if (userData.firstName) {
-		user.firstName = userData.firstName;
-	}
-	if (userData.lastName) {
-		user.lastName = userData.lastName;
-	}
-	return user.update()
+    }
+    if (userData.email) {
+        user.eMail = userData.email
+    }
+    if (userData.firstName) {
+        user.firstName = userData.firstName;
+    }
+    if (userData.lastName) {
+        user.lastName = userData.lastName;
+    }
+    return user.update()
 }
 /**
  * Checks whether a resetToken or an invitationToken is set in the header and if it is valid
@@ -202,26 +202,26 @@ const changeUserData = async (ctx: Context, id: number, isSameUser: boolean, isA
  * @param userData the data sent to change the user data
  */
 const checkToken = async (id: number, ctx: Context, userData: UserParameters) => {
-	let validToken = { valid: false, kind: "" };
-	let invitationToken = ctx.request.headers.get("invitationToken");
-	let resetToken = ctx.request.headers.get("resetToken")
-	logger.error(`invitation: ${JSON.stringify(invitationToken)}`)
-	if (invitationToken) {
-		if (userData.password && userData.firstName) {
-			validToken = { valid: await checkInvitationToken(id, invitationToken, ctx), kind: "invitation" };
-		} else {
-			makeErrorMessage(ctx, 400, "no password and/or firstName provided")
-		}
-	}
-	if (resetToken) {
-		if (userData.password) {
-			validToken = { valid: await checkResetToken(id, resetToken, ctx), kind: "reset" };
-		} else {
-			makeErrorMessage(ctx, 400, "no password provided")
-		}
-	}
+    let validToken = { valid: false, kind: "" };
+    let invitationToken = ctx.request.headers.get("invitationToken");
+    let resetToken = ctx.request.headers.get("resetToken")
+    logger.error(`invitation: ${JSON.stringify(invitationToken)}`)
+    if (invitationToken) {
+        if (userData.password && userData.firstName) {
+            validToken = { valid: await checkInvitationToken(id, invitationToken, ctx), kind: "invitation" };
+        } else {
+            makeErrorMessage(ctx, 400, "no password and/or firstName provided")
+        }
+    }
+    if (resetToken) {
+        if (userData.password) {
+            validToken = { valid: await checkResetToken(id, resetToken, ctx), kind: "reset" };
+        } else {
+            makeErrorMessage(ctx, 400, "no password provided")
+        }
+    }
 
-	return validToken;
+    return validToken;
 }
 
 /**
@@ -231,14 +231,14 @@ const checkToken = async (id: number, ctx: Context, userData: UserParameters) =>
  * @param ctx
  */
 const checkInvitationToken = async (id: number, providedToken: string, ctx: Context) => {
-	let token = await getInvitation(id, providedToken)
-	if (token) {
-		token.delete()
-		return true;
-	} else {
-		makeErrorMessage(ctx, 401, "Invitation token not found")
-		return false;
-	}
+    let token = await getInvitation(id, providedToken)
+    if (token) {
+        token.delete()
+        return true;
+    } else {
+        makeErrorMessage(ctx, 401, "Invitation token not found")
+        return false;
+    }
 }
 
 /**
@@ -248,14 +248,14 @@ const checkInvitationToken = async (id: number, providedToken: string, ctx: Cont
  * @param ctx
  */
 const checkResetToken = async (id: number, providedToken: string, ctx: Context) => {
-	let token = await getResetToken(id, providedToken)
-	if (token) {
-		token.delete()
-		return true;
-	} else {
-		makeErrorMessage(ctx, 401, "not authorized")
-		return false;
-	}
+    let token = await getResetToken(id, providedToken)
+    if (token) {
+        token.delete()
+        return true;
+    } else {
+        makeErrorMessage(ctx, 401, "not authorized")
+        return false;
+    }
 }
 
 /**
@@ -269,25 +269,25 @@ const checkResetToken = async (id: number, providedToken: string, ctx: Context) 
  * @param name name of the person who invited the new user
  */
 const sendInvitationMail = async (jwt: string, linkText: string, email: string, userId: number, client: EMailClient, name?: string) => {
-	if (URL) {
-		let url = urlSanitizer(URL);
-		url += "/register?id=" + userId + "&token=" + jwt;
-		let finalText = linkText.link(url);
-		const content = `Welcome, </br>
+    if (URL) {
+        let url = urlSanitizer(URL);
+        url += "/register?id=" + userId + "&token=" + jwt;
+        let finalText = linkText.link(url);
+        const content = `Welcome, </br>
 		you were invited to join snowballR by ${name ? name : `your snowballR Team`}</br>
         to finalize your registration for snowballR, please visit: ${url}.</br>
         Best Regards, </br>
         Your SnowballR Team`
-		const html = ` <h3> Welcome, </h3>
+        const html = ` <h3> Welcome, </h3>
 		<p> you were invited to join snowballR by ${name ? name : `your snowballR Team`} </p>
         <p> to finalize your registration for snowballR, please visit <a href = "${url}" > ${url} </a></p>
         <p>Best Regards, </p>` +
-			(name ? `<p>${name}</p>` : `<p>your snowballR Team</p>`)
+            (name ? `<p>${name}</p>` : `<p>your snowballR Team</p>`)
 
-		await sendMail(email, client, html, content, "Invitation to join SnowballR", "SnowballR")
-	} else {
-		console.error("no URL in env!")
-	}
+        await sendMail(email, client, html, content, "Invitation to join SnowballR", "SnowballR")
+    } else {
+        console.error("no URL in env!")
+    }
 
 }
 
@@ -298,26 +298,26 @@ const sendInvitationMail = async (jwt: string, linkText: string, email: string, 
  * @param url
  * @param email email of the person wanting to reset his password
  * @param userId
- * @param client email client to send the email with
+ * @param client email client to send the email with 
  */
 const sendResetMail = async (jwt: string, linkText: string, email: string, userId: number, client: EMailClient) => {
-	if (URL) {
-		let url = urlSanitizer(URL);
-		url += "/resetpassword?id=" + userId + "&token=" + jwt;
-		let finalText = linkText.link(url);
-		const content = `Hello, </br>
+    if (URL) {
+        let url = urlSanitizer(URL);
+        url += "/resetpassword?id=" + userId + "&token=" + jwt;
+        let finalText = linkText.link(url);
+        const content = `Hello, </br>
                 to reset your password for snowballR, please visit: ${url}. </br>
                 Best Regards, </br>
                 Your SnowballR Team`
-		const html = `<h3>Welcome, </h3>
+        const html = `<h3>Welcome, </h3>
         <p> to reset your password for snowballR, please visit <a href="${url}">${url}</a></p>
         <p>Best Regards,</p>
         <p>your snowballR Team</p>`
 
-		await sendMail(email, client, html, content, "Password reset for SnowballR", "SnowballR")
-	} else {
-		console.error("no URL in env!")
-	}
+        await sendMail(email, client, html, content, "Password reset for SnowballR", "SnowballR")
+    } else {
+        console.error("no URL in env!")
+    }
 }
 
 /**
@@ -331,17 +331,17 @@ const sendResetMail = async (jwt: string, linkText: string, email: string, userI
  */
 const sendMail = async (mailTo: string, client: EMailClient, html: string, content: string, header: string, name?: string) => {
 
-	await client.connect({
-		hostname: "mail.uni-ulm.de",
-		port: 25,
-	});
-	await client.send({
-		from: name ? `${name} <${adminMail}>` : adminMail,
-		to: mailTo,
-		subject: header,
-		content: content,
-		html: html
-	})
+    await client.connect({
+        hostname: "mail.uni-ulm.de",
+        port: 25,
+    });
+    await client.send({
+        from: name ? `${name} <${adminMail}>` : adminMail,
+        to: mailTo,
+        subject: header,
+        content: content,
+        html: html
+    })
 
-	await client.close();
+    await client.close();
 }
