f10550734c152445f1099627cf224864bb5e5360
Lukas Romer <lukas.romer@uni-ulm.de>
2022-04-27 22:44:43 +0200

    working migration

--- db/migrations/20220318085430_add_id.ts
+++ db/migrations/20220318085430_add_id.ts
@@ -6,10 +6,13 @@ import {
 
 export default class extends AbstractMigration<ClientPostgreSQL> {
 	async up({ dialect }: Info): Promise<void> {
-		await this.client.queryArray("ALTER TABLE papers ADD COLUMN IF NOT EXISTS custom_id VARCHAR;");
+		await this.client.queryArray('ALTER TABLE "paper" ADD COLUMN IF NOT EXISTS "custom_id" VARCHAR;');
 	}
 
 	async down({ dialect }: Info): Promise<void> {
-		await this.client.queryArray("ALTER TABLE papers DROP COLUMN custom_id;");
+		await this.client.queryArray('ALTER TABLE "paper" DROP COLUMN "custom_id";');
 	}
 }
+
+// docker exec -it backend-develop sh -c "cd /nessie && /bin/deno run -A --unstable https://deno.land/x/nessie/cli.ts migrate"
+// docker exec -it backend-develop sh -c "cd /nessie && /bin/deno run -A --unstable https://deno.land/x/nessie/cli.ts rollback"
