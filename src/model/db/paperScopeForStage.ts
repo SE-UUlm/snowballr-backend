04c93b1ed5389d9d987b04a4b04ee84858b63329
Lukas Reiner Wolfram Romer <lukas.romer@uni-ulm.de>
2022-06-22 07:49:50 +0000

    Revert "Merge branch 'feat/fetch_existing_reviews' into 'develop'"
    This reverts merge request !259

--- src/model/db/paperScopeForStage.ts
+++ src/model/db/paperScopeForStage.ts
@@ -1,31 +1,26 @@
-import { DataTypes, Model } from "https://deno.land/x/denodb/mod.ts";
-import { Stage } from "./stage.ts";
-import { Paper } from "./paper.ts";
-import { Review } from "./review.ts";
+import {DataTypes, Model} from "https://deno.land/x/denodb/mod.ts";
+import {Stage} from "./stage.ts";
+import {Paper} from "./paper.ts";
 
 
 export class PaperScopeForStage extends Model {
-	static table = 'inscopefor';
-	static timestamps = true;
+    static table = 'inscopefor';
+    static timestamps = true;
 
-	static fields = {
-		id: { primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER },
-		additionDate: DataTypes.DATE,
-		finalDecision: { type: DataTypes.STRING, allowNull: true },
-	}
+    static fields = {
+        id: {primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER},
+        additionDate: DataTypes.DATE,
+        finalDecision: { type: DataTypes.STRING, allowNull: true },
+    }
 
-	static stage() {
-		return this.hasOne(Stage);
-	}
+    static stage() {
+        return this.hasOne(Stage);
+    }
 
 
-	static paper() {
-		return this.hasOne(Paper);
-	}
-
-	static review() {
-		return this.hasOne(Review);
-	}
+    static paper() {
+        return this.hasOne(Paper);
+    }
 
 
 }
