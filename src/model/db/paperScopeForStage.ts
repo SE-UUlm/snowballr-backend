ae920f7143e2fb7dbb4f471ba1064488870395b6
Lukas Reiner Wolfram Romer <lukas.romer@uni-ulm.de>
2022-06-23 11:48:36 +0000

    Revert "Merge branch 'revert-cf3cfbc0' into 'develop'"
    This reverts merge request !260

--- src/model/db/paperScopeForStage.ts
+++ src/model/db/paperScopeForStage.ts
@@ -1,26 +1,31 @@
-import {DataTypes, Model} from "https://deno.land/x/denodb/mod.ts";
-import {Stage} from "./stage.ts";
-import {Paper} from "./paper.ts";
+import { DataTypes, Model } from "https://deno.land/x/denodb/mod.ts";
+import { Stage } from "./stage.ts";
+import { Paper } from "./paper.ts";
+import { Review } from "./review.ts";
 
 
 export class PaperScopeForStage extends Model {
-    static table = 'inscopefor';
-    static timestamps = true;
+	static table = 'inscopefor';
+	static timestamps = true;
 
-    static fields = {
-        id: {primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER},
-        additionDate: DataTypes.DATE,
-        finalDecision: { type: DataTypes.STRING, allowNull: true },
-    }
+	static fields = {
+		id: { primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER },
+		additionDate: DataTypes.DATE,
+		finalDecision: { type: DataTypes.STRING, allowNull: true },
+	}
 
-    static stage() {
-        return this.hasOne(Stage);
-    }
+	static stage() {
+		return this.hasOne(Stage);
+	}
 
 
-    static paper() {
-        return this.hasOne(Paper);
-    }
+	static paper() {
+		return this.hasOne(Paper);
+	}
+
+	static review() {
+		return this.hasOne(Review);
+	}
 
 
 }
