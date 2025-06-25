74c61be8596af07b4baa6fc6f064c6af0ae932a9
domienderle <dominik.enderle@uni-ulm.de>
2025-06-25 12:43:34 +0200

    feat: add repository layer for delete user

---
+++ src/main/kotlin/repository/UserTableRepo.kt
@@ -0,0 +1,79 @@
+package se.uulm.snowballr.backend.repository
+
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
+import se.uulm.snowballr.backend.db.IDatabase
+import se.uulm.snowballr.backend.model.SnowballRException.NotFoundException
+import se.uulm.snowballr.backend.model.dto.User
+import se.uulm.snowballr.backend.model.parseUUID
+import se.uulm.snowballr.backend.table.UserTable
+import se.uulm.snowballr.backend.table.UserTable.toUser
+import snowballr.UserOuterClass
+import java.time.OffsetDateTime
+
+/**
+ * Defines an interface for repository operations related to the [UserTable].
+ *
+ * This interface is used to handle persistence and retrieval operations for users by providing
+ * abstraction over the underlying database implementation. By using this interface, the logic
+ * for creating and managing users can remain decoupled from the specifics of the database layer.
+ */
+interface IUserTableRepo {
+    suspend fun getUserById(id: String): User
+
+    suspend fun getUserByEmail(email: String): User
+
+    suspend fun getAllUsers(): List<User>
+
+    suspend fun softDeleteUser(id: String): Int
+}
+
+/**
+ * Repository implementation for managing the [UserTable] in the database.
+ *
+ * This class handles the persistence and retrieval of user data by integrating
+ * with the underlying database through the [IDatabase] interface. It provides
+ * concrete methods for CRUD operations on user records within the database.
+ *
+ * @param db The database abstraction used for executing queries within a transaction.
+ */
+class UserTableRepo(
+    private val db: IDatabase,
+) : IUserTableRepo {
+    override suspend fun getUserById(id: String): User = db.dbQuery {
+        val uuid = parseUUID(id, "user")
+
+        UserTable
+            .selectAll()
+            .where { UserTable.id eq uuid }
+            .map { it.toUser() }
+            .singleOrNull()
+            ?: throw NotFoundException.User(id)
+    }
+
+    override suspend fun getUserByEmail(email: String): User = db.dbQuery {
+        UserTable
+            .selectAll()
+            .where { UserTable.email eq email }
+            .map { it.toUser() }
+            .singleOrNull()
+            ?: throw NotFoundException.User(email, "email")
+    }
+
+    override suspend fun getAllUsers(): List<User> =
+        db.dbQuery {
+            UserTable
+                .selectAll()
+                .map { it.toUser() }
+        }
+
+    override suspend fun softDeleteUser(id: String) =
+        db.dbQuery {
+            val uuid = parseUUID(id, "user")
+
+            UserTable.update({ UserTable.id eq uuid }) {
+                it[status] = UserOuterClass.UserStatus.USER_STATUS_DELETED
+                it[deletedAt] = OffsetDateTime.now()
+            }
+        }
+}
---
+++ src/test/kotlin/repository/UserTableRepoTest.kt
@@ -0,0 +1,151 @@
+package se.uulm.snowballr.backend.repository
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import se.uulm.snowballr.backend.model.SnowballRException.InvalidIdException
+import se.uulm.snowballr.backend.model.SnowballRException.NotFoundException
+import se.uulm.snowballr.backend.table.UserTable
+import se.uulm.snowballr.backend.testCoroutine
+import snowballr.UserOuterClass.UserRole
+import snowballr.UserOuterClass.UserStatus
+import java.util.UUID
+
+@ExperimentalCoroutinesApi
+@DelicateCoroutinesApi
+class UserTableRepoTest : H2DatabaseTest(arrayOf(UserTable)) {
+    private val repo = UserTableRepo(db)
+
+    @Nested
+    inner class GetUserById {
+        @Test
+        fun `When a user is found, then the correct user is returned`() = testCoroutine {
+            val userId =
+                db
+                    .dbQuery {
+                        UserTable.insertAndGetId {
+                            it[email] = "test.user@example.com"
+                            it[firstName] = "Test"
+                            it[lastName] = "User"
+                            it[role] = UserRole.USER_ROLE_DEFAULT
+                            it[status] = UserStatus.USER_STATUS_ACTIVE
+                        }
+                    }.value
+
+            val user = repo.getUserById(userId.toString())
+
+            assertThat(user.id).isEqualTo(userId)
+            assertThat(user.email).isEqualTo("test.user@example.com")
+            assertThat(user.firstName).isEqualTo("Test")
+            assertThat(user.lastName).isEqualTo("User")
+            assertThat(user.role).isEqualTo(UserRole.USER_ROLE_DEFAULT)
+            assertThat(user.status).isEqualTo(UserStatus.USER_STATUS_ACTIVE)
+        }
+
+        @Test
+        fun `When a user is not found, then an exception is thrown`() = testCoroutine {
+            assertThrows<NotFoundException.User> { repo.getUserById(UUID.randomUUID().toString()) }
+        }
+
+        @Test
+        fun `When the uuid is invalid, then an exception is thrown`() = testCoroutine {
+            assertThrows<InvalidIdException.UUID> { repo.getUserById("invalid uuid") }
+        }
+    }
+
+    @Nested
+    inner class GetUserByEmail {
+        @Test
+        fun `When a user is found, then the correct user is returned`() = testCoroutine {
+            val userId =
+                db
+                    .dbQuery {
+                        UserTable.insertAndGetId {
+                            it[email] = "test.user@example.com"
+                            it[firstName] = "Test"
+                            it[lastName] = "User"
+                            it[role] = UserRole.USER_ROLE_DEFAULT
+                            it[status] = UserStatus.USER_STATUS_ACTIVE
+                        }
+                    }.value
+
+            val user = repo.getUserByEmail("test.user@example.com")
+
+            assertThat(user.id).isEqualTo(userId)
+            assertThat(user.email).isEqualTo("test.user@example.com")
+            assertThat(user.firstName).isEqualTo("Test")
+            assertThat(user.lastName).isEqualTo("User")
+            assertThat(user.role).isEqualTo(UserRole.USER_ROLE_DEFAULT)
+            assertThat(user.status).isEqualTo(UserStatus.USER_STATUS_ACTIVE)
+        }
+
+        @Test
+        fun `When a user is not found, then an exception is thrown`() = testCoroutine {
+            assertThrows<NotFoundException.User> { repo.getUserByEmail("non-existing email") }
+        }
+    }
+
+    @Nested
+    inner class GetAllUsers {
+        @Test
+        fun `When users are found, then all users are returned`() = testCoroutine {
+            val userId1 =
+                db
+                    .dbQuery {
+                        UserTable.insertAndGetId {
+                            it[email] = "test.user1@example.com"
+                            it[firstName] = "Test"
+                            it[lastName] = "User 2"
+                            it[role] = UserRole.USER_ROLE_DEFAULT
+                            it[status] = UserStatus.USER_STATUS_ACTIVE
+                        }
+                    }.value
+
+            val userId2 =
+                db
+                    .dbQuery {
+                        UserTable.insertAndGetId {
+                            it[email] = "test.user2@example.com"
+                            it[firstName] = "Test"
+                            it[lastName] = "User 1"
+                            it[role] = UserRole.USER_ROLE_DEFAULT
+                            it[status] = UserStatus.USER_STATUS_ACTIVE
+                        }
+                    }.value
+
+            val users = repo.getAllUsers()
+            assertThat(users).hasSize(2)
+            val firstUser = users.find { it.id == userId1 }
+            assertThat(firstUser).isNotNull
+            val secondUser = users.find { it.id == userId2 }
+            assertThat(secondUser).isNotNull
+        }
+    }
+
+    @Nested
+    inner class DeleteUser {
+        @Test
+        fun `When the user is found, then the status of the user is set to USER_STATUS_DELETED`() =
+            testCoroutine {
+                val userId1 =
+                    db
+                        .dbQuery {
+                            UserTable.insertAndGetId {
+                                it[email] = "test.user@example.com"
+                                it[firstName] = "Test"
+                                it[lastName] = "User"
+                                it[role] = UserRole.USER_ROLE_DEFAULT
+                                it[status] = UserStatus.USER_STATUS_ACTIVE
+                            }
+                        }.value
+
+                repo.softDeleteUser(userId1.toString())
+                val deletedUser = repo.getUserById(userId1.toString())
+                assertThat(deletedUser.status).isEqualTo(UserStatus.USER_STATUS_DELETED)
+            }
+    }
+}
