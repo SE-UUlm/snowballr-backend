bc4276fee1a737b7a069c42a2ea030bbeb261594
Felix Schlegel <felix.schlegel@gmx.de>
2025-06-28 19:44:37 +0200

    test: use test helper consistently

--- src/test/kotlin/repository/association/ProjectMemberTableRepoTest.kt
+++ src/test/kotlin/repository/association/ProjectMemberTableRepoTest.kt
@@ -27,7 +27,7 @@ class ProjectMemberTableRepoTest : H2DatabaseTest(arrayOf(ProjectTable, ProjectM
     private val repo = ProjectMemberTableRepo(db)
     private val projectRepo = ProjectTableRepo(db)
 
-    suspend fun createExampleProject(): Project {
+    private suspend fun createExampleProject(): Project {
         val request =
             ProjectOuterClass.Project.Create
                 .newBuilder()
@@ -36,7 +36,7 @@ class ProjectMemberTableRepoTest : H2DatabaseTest(arrayOf(ProjectTable, ProjectM
         return projectRepo.createProject(request, testUserId)
     }
 
-    suspend fun addTestMember(index: Int, projectId: UUID): ProjectMember {
+    private suspend fun addTestMember(index: Int, projectId: UUID): ProjectMember {
         val userId =
             db.dbQuery {
                 UserTable
@@ -51,40 +51,51 @@ class ProjectMemberTableRepoTest : H2DatabaseTest(arrayOf(ProjectTable, ProjectM
         return repo.addUserToProject(userId, projectId)
     }
 
-    suspend fun setupProject(): Project {
+    private suspend fun setupProject(
+        numberOfTestMembers: Int = 0,
+        addTestUser: Boolean = true,
+    ): Pair<Project, List<ProjectMember>> {
         val project = createExampleProject()
-        repo.addUserToProject(testUserId, project.id)
+        val members = mutableListOf<ProjectMember>()
 
-        return project
+        if (addTestUser) {
+            repo.addUserToProject(testUserId, project.id).also { members.add(it) }
+        }
+
+        for (i in 1..numberOfTestMembers) {
+            addTestMember(i, project.id).also { members.add(it) }
+        }
+
+        return project to members
     }
 
     @Nested
     inner class AddUserToProject {
         @Test
         fun `When a user is added to a project, then they can be retrieved as project member`() = testCoroutine {
-            val project = createExampleProject()
-
-            val member = repo.addUserToProject(testUserId, project.id)
+            val (project, members) = setupProject()
+            val member = members.first()
 
             assertThat(member.userId).isEqualTo(testUserId)
             assertThat(member.projectId).isEqualTo(project.id)
             assertThat(member.role).isEqualTo(MemberRole.MEMBER_ROLE_DEFAULT)
-            val members = repo.getMembersOfProject(project.id)
-            assertThat(members).hasSize(1)
-            assertThat(members).containsExactly(member)
+
+            val actualMembers = repo.getMembersOfProject(project.id)
+            assertThat(actualMembers).hasSize(1)
+            assertThat(actualMembers).containsExactly(member)
         }
 
         @Test
         fun `When a user is added to a project twice, then only one member is created`() = testCoroutine {
-            val project = createExampleProject()
-
-            val member = repo.addUserToProject(testUserId, project.id)
+            val (project, members) = setupProject()
+            val member = members.first()
             val member1 = repo.addUserToProject(testUserId, project.id)
 
             assertThat(member).isEqualTo(member1)
-            val members = repo.getMembersOfProject(project.id)
-            assertThat(members).hasSize(1)
-            assertThat(members).containsExactly(member)
+
+            val actualMembers = repo.getMembersOfProject(project.id)
+            assertThat(actualMembers).hasSize(1)
+            assertThat(actualMembers).containsExactly(member)
         }
 
         @Test
@@ -109,7 +120,7 @@ class ProjectMemberTableRepoTest : H2DatabaseTest(arrayOf(ProjectTable, ProjectM
 inner class GetMembersOfProject {
     @Test
     fun `When no members are in a project, then the list is empty`() = testCoroutine {
-        val project = createExampleProject()
+        val (project, _) = setupProject(0, false)
 
         val members = repo.getMembersOfProject(project.id)
 
@@ -117,30 +128,30 @@ inner class GetMembersOfProject {
     }
 
     @Test
-    fun `When a member is in a project, then they are part of the list`() = testCoroutine {
-        val project = setupProject()
-        val member1 = addTestMember(1, project.id)
-        val member2 = addTestMember(2, project.id)
+    fun `When one member is in a project, then they are part of the list`() = testCoroutine {
+        val (project, _) = setupProject()
 
-        val members = repo.getMembersOfProject(project.id)
+        val actualMembers = repo.getMembersOfProject(project.id)
 
-        assertThat(members).hasSize(3)
-        val member = members.first()
+        assertThat(actualMembers).hasSize(1)
+        val member = actualMembers.first()
         assertThat(member.userId).isEqualTo(testUserId)
-        assertThat(member1).isEqualTo(members[1])
-        assertThat(member2).isEqualTo(members[2])
     }
 
     @Test
     fun `When several members are in a project, then they are part of the list`() = testCoroutine {
-        val project = setupProject()
+        val (project, members) = setupProject(3)
 
-        val members = repo.getMembersOfProject(project.id)
+        val actualMembers = repo.getMembersOfProject(project.id)
 
-        assertThat(members).hasSize(1)
-        val member = members.first()
-        assertThat(member.userId).isEqualTo(testUserId)
+        assertThat(actualMembers).hasSize(4)
+        assertThat(actualMembers[0].userId).isEqualTo(testUserId)
+        assertThat(actualMembers[0]).isEqualTo(members[0])
+        assertThat(actualMembers[1]).isEqualTo(members[1])
+        assertThat(actualMembers[2]).isEqualTo(members[2])
+        assertThat(actualMembers[3]).isEqualTo(members[3])
     }
+}
 }
 
 @Nested
@@ -163,9 +174,9 @@ inner class GetMembersInSameProjectsAsUser {
 
     @Test
     fun `When the user is in projects with other users, then all members are part of the list`() = testCoroutine {
-        val project1 = setupProject()
-        val project2 = setupProject()
-        val project3 = setupProject()
+        val project1 = setupProject().first
+        val project2 = setupProject().first
+        val project3 = setupProject().first
 
         val member1 = addTestMember(1, project1.id)
         val member2 = addTestMember(2, project2.id)
