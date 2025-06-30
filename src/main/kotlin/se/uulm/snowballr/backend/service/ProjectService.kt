f69c23f4ae602ec0716f6c8343a9e2f182d6490b
Felix Schlegel <felix.schlegel@gmx.de>
2025-06-30 16:39:34 +0200

    feat: add service implementation for getAllProjects

--- src/main/kotlin/se/uulm/snowballr/backend/service/ProjectService.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/service/ProjectService.kt
@@ -2,16 +2,24 @@ package se.uulm.snowballr.backend.service
 
 import se.uulm.snowballr.backend.db.dummyUserId
 import se.uulm.snowballr.backend.grpc.SnowballRServer.SnowballRService
+import se.uulm.snowballr.backend.model.SnowballRException.UnauthorizedException
 import se.uulm.snowballr.backend.model.dto.toGrpcProject
 import se.uulm.snowballr.backend.model.parseUUID
 import se.uulm.snowballr.backend.repository.IProjectTableRepo
+import se.uulm.snowballr.backend.repository.IUserTableRepo
 import snowballr.ProjectOuterClass
+import snowballr.UserOuterClass.UserRole
 
 interface IProjectService {
     /**
      * Service implementation of [SnowballRService.createProject].
      */
     suspend fun createProject(request: ProjectOuterClass.Project.Create): ProjectOuterClass.Project
+
+    /**
+     * Service implementation of [SnowballRService.getAllProjects].
+     */
+    suspend fun getAllProjects(): ProjectOuterClass.Project.List
 }
 
 /**
@@ -22,13 +30,31 @@ interface IProjectService {
  *
  * @constructor Initializes the [ProjectService] with a project repository.
  * @param repo The repository responsible for managing persistence operations for projects.
+ * @param userRepo The repository responsible for managing persistence operations for users.
  */
 class ProjectService(
     private val repo: IProjectTableRepo,
+    private val userRepo: IUserTableRepo,
 ) : IProjectService {
     override suspend fun createProject(request: ProjectOuterClass.Project.Create): ProjectOuterClass.Project {
         // TODO: remove dummy user when user management is implemented
         val requestingUserId = parseUUID(dummyUserId!!, "user")
         return repo.createProject(request, requestingUserId).toGrpcProject()
+    }
+
+    override suspend fun getAllProjects(): ProjectOuterClass.Project.List {
+        val requestingUserId = parseUUID(dummyUserId!!, "user")
+        val currentUser = userRepo.getUserById(requestingUserId)
+        // Check whether the current user has access to retrieve all projects
+        // TODO: remove dummy user when user management is implemented
+        if (currentUser.role != UserRole.USER_ROLE_ADMIN) {
+            throw UnauthorizedException.All.Project(dummyUserId!!)
+        }
+
+        val projects = repo.getAllProjects()
+
+        val builder = ProjectOuterClass.Project.List.newBuilder()
+        projects.forEach { builder.addProjects(it.toGrpcProject()) }
+        return builder.build()
     }
 }
---
+++ src/test/kotlin/se/uulm/snowballr/backend/service/project/GetAllProjectsTest.kt
@@ -0,0 +1,56 @@
+package se.uulm.snowballr.backend.service.project
+
+import io.mockk.coEvery
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import se.uulm.snowballr.backend.DataBuilder
+import se.uulm.snowballr.backend.TestSpecificException
+import se.uulm.snowballr.backend.model.SnowballRException.UnauthorizedException
+import se.uulm.snowballr.backend.service.MainServiceTest
+import se.uulm.snowballr.backend.testCoroutine
+import snowballr.UserOuterClass.UserRole
+
+@ExperimentalCoroutinesApi
+@DelicateCoroutinesApi
+internal class GetAllProjectsTest : MainServiceTest() {
+    @Test
+    fun `When all projects are retrieved by an admin, then no exception is thrown`() = testCoroutine {
+        val adminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
+
+        coEvery { userRepoMock.getUserById(any()) } returns adminUser
+        coEvery { projectRepoMock.getAllProjects() } returns emptyList()
+
+        assertDoesNotThrow { mainService.getAllProjects() }
+    }
+
+    @Test
+    fun `When retrieving the current user fails, then an exception is thrown`() = testCoroutine {
+        coEvery { userRepoMock.getUserById(any()) } throws TestSpecificException()
+        coEvery { projectRepoMock.getAllProjects() } returns emptyList()
+
+        assertThrows<TestSpecificException> { mainService.getAllProjects() }
+    }
+
+    @Test
+    fun `When all projects are retrieved by a non-admin, then an exception is thrown`() = testCoroutine {
+        val nonAdminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
+
+        coEvery { userRepoMock.getUserById(any()) } returns nonAdminUser
+        coEvery { projectRepoMock.getAllProjects() } returns emptyList()
+
+        assertThrows<UnauthorizedException.All.Project> { mainService.getAllProjects() }
+    }
+
+    @Test
+    fun `When retrieving all projects fails, then an exception is thrown`() = testCoroutine {
+        val adminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
+
+        coEvery { userRepoMock.getUserById(any()) } returns adminUser
+        coEvery { projectRepoMock.getAllProjects() } throws TestSpecificException()
+
+        assertThrows<TestSpecificException> { mainService.getAllProjects() }
+    }
+}
