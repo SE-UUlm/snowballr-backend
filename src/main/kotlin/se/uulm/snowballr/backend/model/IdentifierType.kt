f50d3efa8051540da294e910e518cd74d0cc1637
Felix Schlegel <felix.schlegel@gmx.de>
2025-07-03 10:47:52 +0200

    feat: add IdentifierType enum

---
+++ src/main/kotlin/se/uulm/snowballr/backend/model/IdentifierType.kt
@@ -0,0 +1,9 @@
+package se.uulm.snowballr.backend.model
+
+/**
+ * Enum that represents different identifiers for entities.
+ */
+enum class IdentifierType(val displayName: String) {
+    ID("ID"),
+    EMAIL("email"),
+}
