0699d645048e31b7c46b47bddcb508644072ff38
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-07-04 16:27:50 +0200

    test: add missing mocks of functions called in the updateUser function in the service layer

--- src/test/kotlin/se/uulm/snowballr/backend/service/user/UpdateUserTest.kt
+++ src/test/kotlin/se/uulm/snowballr/backend/service/user/UpdateUserTest.kt
@@ -4,21 +4,33 @@ import com.google.protobuf.util.FieldMaskUtil
 import io.mockk.coEvery
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import se.uulm.snowballr.backend.DataBuilder
 import se.uulm.snowballr.backend.TestSpecificException
+import se.uulm.snowballr.backend.db.dummyUserId
 import se.uulm.snowballr.backend.model.SnowballRException
 import se.uulm.snowballr.backend.model.dto.toGrpcUser
 import se.uulm.snowballr.backend.service.MainServiceTest
 import se.uulm.snowballr.backend.testCoroutine
 import snowballr.UserOuterClass.User
 import snowballr.UserOuterClass.UserRole
+import java.util.UUID
 
 @ExperimentalCoroutinesApi
 @DelicateCoroutinesApi
 class UpdateUserTest : MainServiceTest() {
+    @BeforeEach
+    override fun setUpTest() {
+        super.setUpTest()
+
+        coEvery { userRepoMock.getUserById(any()) } throws NotImplementedError()
+        coEvery { userRepoMock.doesUserExistByEmail(any()) } throws NotImplementedError()
+        coEvery { userRepoMock.updateUser(any()) } throws NotImplementedError()
+    }
+
     @Test
     fun `When a user updates their own information successfully, then no exception is thrown`() = testCoroutine {
         val nonAdminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
@@ -26,11 +38,13 @@ class UpdateUserTest : MainServiceTest() {
             id = nonAdminUser.id,
             firstName = "John",
             lastName = "Doe",
-        ).toGrpcUser()
+        )
         val updateFieldMask = FieldMaskUtil.fromStringList(listOf("first_name", "last_name"))
-        val request = User.Update.newBuilder().setUser(updatedUser).setMask(updateFieldMask).build()
+        val request = User.Update.newBuilder().setUser(updatedUser.toGrpcUser()).setMask(updateFieldMask).build()
 
         coEvery { userRepoMock.getUserById(any()) } returns nonAdminUser
+        coEvery { userRepoMock.doesUserExistByEmail(any()) } returns false
+        coEvery { userRepoMock.updateUser(request) } returns updatedUser
 
         assertDoesNotThrow { mainService.updateUser(request) }
     }
@@ -42,11 +56,13 @@ class UpdateUserTest : MainServiceTest() {
             email = "john@doe.com",
             firstName = "John",
             lastName = "Doe",
-        ).toGrpcUser()
+        )
         val updateFieldMask = FieldMaskUtil.fromStringList(listOf("email", "first_name", "last_name"))
-        val request = User.Update.newBuilder().setUser(updatedUser).setMask(updateFieldMask).build()
+        val request = User.Update.newBuilder().setUser(updatedUser.toGrpcUser()).setMask(updateFieldMask).build()
 
         coEvery { userRepoMock.getUserById(any()) } returns adminUser
+        coEvery { userRepoMock.doesUserExistByEmail(any()) } returns false
+        coEvery { userRepoMock.updateUser(request) } returns updatedUser
 
         assertDoesNotThrow { mainService.updateUser(request) }
     }
@@ -78,12 +94,12 @@ class UpdateUserTest : MainServiceTest() {
     @Test
     fun `When a non-admin user wants to change the user information from another user, then an exception is thrown`() =
         testCoroutine {
-            val anotherUser = DataBuilder.createExampleUser()
+            val anotherUser = DataBuilder.createExampleUser(id = UUID.fromString(dummyUserId))
             val updatedUser = DataBuilder.createExampleUser()
             val updateFieldMask = FieldMaskUtil.fromString("first_name")
             val request = User.Update.newBuilder().setUser(updatedUser.toGrpcUser()).setMask(updateFieldMask).build()
 
-            coEvery { userRepoMock.getUserById(anotherUser.id) } returns anotherUser
+            coEvery { userRepoMock.getUserById(UUID.fromString(dummyUserId)) } returns anotherUser
             coEvery { userRepoMock.getUserById(updatedUser.id) } returns updatedUser
 
             assertThrows<SnowballRException.UnauthorizedException.Single> { mainService.updateUser(request) }
@@ -96,6 +112,7 @@ class UpdateUserTest : MainServiceTest() {
         val request = User.Update.newBuilder().setUser(updatedUser).build()
 
         coEvery { userRepoMock.getUserById(any()) } returns adminUser
+        coEvery { userRepoMock.doesUserExistByEmail(any()) } returns false
         coEvery { userRepoMock.updateUser(any()) } throws TestSpecificException()
 
         assertThrows<TestSpecificException> { mainService.updateUser(request) }
