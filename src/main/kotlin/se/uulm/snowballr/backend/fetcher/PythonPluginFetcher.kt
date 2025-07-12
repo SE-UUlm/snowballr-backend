2c8242af31ec2191d0852ee2ace760e21967bf35
Luca Schlecker <luca@rfts.me>
2025-07-12 20:54:05 +0200

    feat(PythonPluginFetcher): add conversions functions and accompanying tests

--- src/main/kotlin/se/uulm/snowballr/backend/fetcher/PythonPluginFetcher.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/fetcher/PythonPluginFetcher.kt
@@ -1,52 +1,112 @@
 package se.uulm.snowballr.backend.fetcher
 
-import jep.SharedInterpreter
-import jep.MainInterpreter
-import se.uulm.snowballr.backend.model.dto.Paper
 import io.github.oshai.kotlinlogging.KotlinLogging
+import jep.Jep
+import jep.MainInterpreter
+import jep.SharedInterpreter
+import jep.python.PyBuiltins
+import jep.python.PyObject
+import kotlinx.datetime.Instant
+import se.uulm.snowballr.backend.model.dto.Paper
+import java.time.OffsetDateTime
+import java.util.HashMap
+import java.util.UUID
 
 private val logger = KotlinLogging.logger {}
+
+@Suppress("StringTemplateIndent", "Indentation")
+val jepLocatorScript = """
+    import site
+    import os
+    import glob
+    for f in glob.glob(os.path.join(site.getsitepackages()[0], "jep/libjep.*")):
+        print(f)
+    """.trimIndent()
+
+@Suppress("StringTemplateIndent", "Indentation")
+private val pythonDataTypes = """
+    from dataclasses import dataclass
+    from datetime import datetime
+    from typing import Optional
+
+    @dataclass
+    class Paper:
+        title: str
+        abstract: str
+        externalId: Optional[str] = None
+        publishedAt: Optional[int] = None
+        publisher: Optional[str] = None
+        publicationType: Optional[str] = None
+        publicationName: Optional[str] = None
+    """.trimIndent()
 
 class PythonPluginFetcher : IFetcher {
     companion object {
         fun locateNativeLibrary() {
-            val locatorScript = """
-                import site
-                import os
-                import glob
-                for f in glob.glob(os.path.join(site.getsitepackages()[0], "jep/libjep.*")):
-                    print(f)
-            """.trimIndent()
             val process = ProcessBuilder()
                 .command("python3", "-")
                 .redirectInput(ProcessBuilder.Redirect.PIPE)
                 .redirectOutput(ProcessBuilder.Redirect.PIPE)
                 .start()
 
-            with (process.outputStream) {
-                write(locatorScript.toByteArray(Charsets.UTF_8))
+            with(process.outputStream) {
+                write(jepLocatorScript.toByteArray(Charsets.UTF_8))
                 close()
             }
 
             val ret = process.inputStream.bufferedReader().readLine()
-            logger.info { "Located Jep C Library: ${ret}" }
+            logger.info { "Located Jep C Library: $ret" }
             MainInterpreter.setJepLibraryPath(ret)
         }
+
+        fun newInterpreter(): Jep {
+            val interp = SharedInterpreter()
+            interp.exec(pythonDataTypes)
+            return interp
+        }
+
+        fun withNewInterpreter(block: (Jep) -> Unit) = newInterpreter().use(block)
     }
 
-    override suspend fun getAvailableOptions(): Set<String> {
-        return setOf()
-    }
 
-    override suspend fun searchPapers(searchQuery: String, options: Map<String, String>): Set<Paper> {
-        return setOf()
-    }
+    override suspend fun getAvailableOptions(): Set<String> = setOf()
 
-    override suspend fun fetchForwardReferences(paper: Paper, options: Map<String, String>): Set<Paper> {
-        return setOf()
-    }
+    override suspend fun searchPapers(searchQuery: String, options: Map<String, String>): Set<Paper> = setOf()
 
-    override suspend fun fetchBackwardReferences(paper: Paper, options: Map<String, String>): Set<Paper> {
-        return setOf()
-    }
+    override suspend fun fetchForwardReferences(paper: Paper, options: Map<String, String>): Set<Paper> = setOf()
+
+    override suspend fun fetchBackwardReferences(paper: Paper, options: Map<String, String>): Set<Paper> = setOf()
+}
+
+fun Map<String, String>.toPyObject(interp: Jep): PyObject = PyBuiltins.get(interp).dict(this)
+
+fun PyObject.toOptionsMap(): Map<String, String> = this.`as`(HashMap<String, String>()::class.java)
+
+fun PyObject.toPaper(): Paper = Paper(
+    UUID.randomUUID(),
+    this.getAttr("title", String::class.java),
+    this.getAttr("externalId", String::class.java),
+    this.getAttr("abstract", String::class.java),
+    this.getAttr("publishedAt", Integer::class.java)?.toLong()?.let { Instant.fromEpochSeconds(it) },
+    this.getAttr("publisher", String::class.java),
+    this.getAttr("publicationType", String::class.java),
+    this.getAttr("publicationName", String::class.java),
+    null,
+    OffsetDateTime.now(),
+    null,
+    null,
+)
+
+fun Paper.toPyObject(interp: Jep): PyObject {
+    val paper = interp.getValue("""Paper("", "")""", PyObject::class.java)
+
+    paper.setAttr("title", this.title)
+    paper.setAttr("abstract", this.abstract)
+    paper.setAttr("externalId", this.externalId)
+    paper.setAttr("publishedAt", this.publishedAt?.epochSeconds)
+    paper.setAttr("publisher", this.publisher)
+    paper.setAttr("publicationType", this.publicationType)
+    paper.setAttr("publicationName", this.publicationName)
+
+    return paper
 }
---
+++ src/test/kotlin/se/uulm/snowballr/backend/fetcher/PythonPluginFetcherTest.kt
@@ -0,0 +1,121 @@
+package se.uulm.snowballr.backend.fetcher
+
+import jep.python.PyObject
+import kotlinx.datetime.Instant
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import se.uulm.snowballr.backend.model.dto.Paper
+import java.time.OffsetDateTime
+import java.util.UUID
+
+private val examplePaper = Paper(
+    UUID.randomUUID(),
+    "Title",
+    "ExternalId",
+    "Abstract",
+    Instant.fromEpochSeconds(0),
+    "Publisher",
+    "PublicationType",
+    "PublicationName",
+    UUID.randomUUID(),
+    OffsetDateTime.now(),
+    null,
+    null,
+)
+
+internal class PythonPluginFetcherTest {
+    companion object {
+        @BeforeAll
+        fun locateJepLibrary() {
+            PythonPluginFetcher.locateNativeLibrary()
+        }
+    }
+
+    @Test
+    fun `When an options-map is convert to a PyObject, python should be able to correctly access it`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            val opts = mapOf("foo" to "bar")
+            interp.set("opts", opts.toPyObject(interp))
+            assertEquals("bar", interp.getValue("""opts["foo"]""", String::class.java))
+        }
+    }
+
+    @Test
+    fun `When a python dictionary is convert to an options-map, java should be able to correctly access it`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            val opts = interp.getValue("""{"foo": "bar"}""", PyObject::class.java).toOptionsMap()
+            assertEquals("bar", opts.get("foo"))
+        }
+    }
+
+    @Test
+    fun `When a options-map is convert to a PyObject and back, it should stay the same`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            val options = mapOf("foo" to "bar")
+            assertEquals(options, options.toPyObject(interp).toOptionsMap())
+        }
+    }
+
+    @Test
+    fun `When a paper is convert to a python paper, python should be able to correctly access it`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            interp.set("paper", examplePaper.toPyObject(interp))
+            assertEquals(examplePaper.title, interp.getValue("paper.title", String::class.java))
+            assertEquals(examplePaper.externalId, interp.getValue("paper.externalId", String::class.java))
+            assertEquals(examplePaper.abstract, interp.getValue("paper.abstract", String::class.java))
+            assertEquals(
+                examplePaper.publishedAt?.epochSeconds,
+                interp.getValue("paper.publishedAt", Integer::class.java)?.toLong(),
+            )
+            assertEquals(examplePaper.publisher, interp.getValue("paper.publisher", String::class.java))
+            assertEquals(examplePaper.publicationType, interp.getValue("paper.publicationType", String::class.java))
+            assertEquals(examplePaper.publicationName, interp.getValue("paper.publicationName", String::class.java))
+        }
+    }
+
+    @Test
+    fun `When a python paper is convert to a paper, java should be able to correctly access it`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            val paper = interp.getValue(
+                """
+                Paper(
+                    "Title",
+                    "Abstract",
+                    "ExternalId",
+                    0,
+                    "Publisher",
+                    "PublicationType",
+                    "PublicationName"
+                )
+                """.trimIndent(),
+                PyObject::class.java,
+            ).toPaper()
+            with(paper) {
+                assertEquals("Title", title)
+                assertEquals("Abstract", abstract)
+                assertEquals("ExternalId", externalId)
+                assertEquals(0, publishedAt?.epochSeconds)
+                assertEquals("Publisher", publisher)
+                assertEquals("PublicationType", publicationType)
+                assertEquals("PublicationName", publicationName)
+            }
+        }
+    }
+
+    @Test
+    fun `When a paper is convert to a python paper and back, it should stay the same`() {
+        PythonPluginFetcher.withNewInterpreter { interp ->
+            val converted = examplePaper.toPyObject(interp).toPaper()
+            with(converted) {
+                assertEquals(examplePaper.title, title)
+                assertEquals(examplePaper.abstract, abstract)
+                assertEquals(examplePaper.externalId, externalId)
+                assertEquals(examplePaper.publishedAt?.epochSeconds, publishedAt?.epochSeconds)
+                assertEquals(examplePaper.publisher, publisher)
+                assertEquals(examplePaper.publicationType, publicationType)
+                assertEquals(examplePaper.publicationName, publicationName)
+            }
+        }
+    }
+}
