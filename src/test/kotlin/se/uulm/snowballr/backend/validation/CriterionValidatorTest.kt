0b84fafaecf47f7d6f64239f7369872ab09adac0
Felix Schlegel <felix.schlegel@gmx.de>
2025-07-27 16:34:47 +0200

    chore: apply UseEmptyCounterpart rule

--- src/test/kotlin/se/uulm/snowballr/backend/validation/CriterionValidatorTest.kt
+++ src/test/kotlin/se/uulm/snowballr/backend/validation/CriterionValidatorTest.kt
@@ -128,7 +128,7 @@ class CriterionValidatorTest {
 
         @Test
         fun `When a blank field mask is validated, then the 'InvalidFieldMask' issue is returned`() {
-            val inValidFieldMask = FieldMaskUtil.fromStringList(listOf())
+            val inValidFieldMask = FieldMaskUtil.fromStringList(emptyList())
             val request =
                 validUpdateRequestBuilder
                     .setMask(inValidFieldMask)
