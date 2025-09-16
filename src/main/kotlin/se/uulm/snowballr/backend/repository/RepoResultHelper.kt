b6198263d171aef5172396b37aba43f2d4c7bfd4
Felix Schlegel <felix.schlegel@gmx.de>
2025-09-16 17:39:43 +0200

    refactor: rename Key to KeyT

--- src/main/kotlin/se/uulm/snowballr/backend/repository/RepoResultHelper.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/repository/RepoResultHelper.kt
@@ -8,17 +8,17 @@ import se.uulm.snowballr.backend.model.SnowballRException.NotFoundException
  * Returns an entity by its key encapsulated by a [Result]. If the entity couldn't be found, a [Result.Failure] is
  * returned, containing a [NotFoundException].
  *
- * @param Key The type of the key.
+ * @param KeyT The type of the key.
  * @param EntT The type of the entity.
  * @param getter The actual getter method that returns a nullable entity.
  * @param entityType The [EntityType] of the entity.
  * @param key The key of the entity.
  * @param identifierType The [IdentifierType] of the [key].
  */
-fun <Key : Any, EntT> getEntityByKeyAsResult(
-    getter: (Key) -> EntT?,
+fun <KeyT : Any, EntT> getEntityByKeyAsResult(
+    getter: (KeyT) -> EntT?,
     entityType: EntityType,
-    key: Key,
+    key: KeyT,
     identifierType: IdentifierType = IdentifierType.ID,
 ): Result<EntT> {
     val entity = getter(key)
@@ -34,18 +34,18 @@ fun <Key : Any, EntT> getEntityByKeyAsResult(
  * Returns an entity by its two keys encapsulated by a [Result]. If the entity couldn't be found, a [Result.Failure] is
  * returned, containing a [NotFoundException].
  *
- * @param Key The type of the key.
+ * @param KeyT The type of the key.
  * @param EntT The type of the entity.
  * @param getter The actual getter method that returns a nullable entity.
  * @param entityType The [EntityType] of the entity.
  * @param key1 The first key of the entity.
  * @param key2 The second key of the entity.
  */
-fun <Key : Any, EntT> getEntityByKeysAsResult(
-    getter: (Key, Key) -> EntT?,
+fun <KeyT : Any, EntT> getEntityByKeysAsResult(
+    getter: (KeyT, KeyT) -> EntT?,
     entityType: EntityType,
-    key1: Key,
-    key2: Key,
+    key1: KeyT,
+    key2: KeyT,
 ): Result<EntT> {
     val entity = getter(key1, key2)
 
