e445670311001be3ad8298d844df164dddfa89cc
Moritz Wieland <moritz.wieland@googlemail.com>
2026-02-20 16:11:51 +0100

    test: improve snowballr exception coverage

---
+++ src/test/kotlin/se/uulm/snowballr/backend/model/exception/SnowballRExceptionTest.kt
@@ -0,0 +1,16 @@
+package se.uulm.snowballr.backend.model.exception
+
+import io.grpc.Status
+import org.junit.jupiter.api.Test
+import se.uulm.snowballr.backend.model.EntityType
+import se.uulm.snowballr.backend.model.exception.invalidargument.InvalidUUIDException
+import kotlin.test.assertEquals
+
+class SnowballRExceptionTest {
+    @Test
+    fun `When getting grpc status from a SnowballR exception, then the configured status is returned`() {
+        val exception = InvalidUUIDException(EntityType.USER, "invalid-uuid")
+
+        assertEquals(Status.INVALID_ARGUMENT, exception.getGrpcStatus())
+    }
+}
