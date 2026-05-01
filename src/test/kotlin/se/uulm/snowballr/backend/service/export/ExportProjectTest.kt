982ef560451c732f662f76f1f4fa0400841e8e8f
Felix Schlegel <felix.schlegel@gmx.de>
2026-05-01 21:10:26 +0200

    test: don't test every access path for the ExportService

--- src/test/kotlin/se/uulm/snowballr/backend/service/export/ExportProjectTest.kt
+++ src/test/kotlin/se/uulm/snowballr/backend/service/export/ExportProjectTest.kt
@@ -1,6 +1,7 @@
 package se.uulm.snowballr.backend.service.export
 
 import io.mockk.coEvery
+import io.mockk.coJustRun
 import io.mockk.every
 import io.mockk.mockkObject
 import kotlinx.coroutines.test.runTest
@@ -9,8 +10,6 @@ import org.junit.jupiter.api.assertThrows
 import se.uulm.snowballr.backend.DataBuilder
 import se.uulm.snowballr.backend.TestSpecificException
 import se.uulm.snowballr.backend.export.ProjectExportManager
-import se.uulm.snowballr.backend.model.exception.notfound.entity.ProjectNotFoundException
-import se.uulm.snowballr.backend.model.exception.unauthorized.UnauthorizedReadException
 import se.uulm.snowballr.backend.model.export.ExportFormat
 import se.uulm.snowballr.backend.model.export.FileExport
 import se.uulm.snowballr.backend.service.MainServiceTest
@@ -26,7 +25,7 @@ class ExportProjectTest : MainServiceTest() {
     }
 
     @Test
-    fun `When exporting a project is successful, then no exception is thrown`() = runTest {
+    fun `When the user exports a project and has access, then no exception is thrown`() = runTest {
         val user = DataBuilder.createExampleUser()
         val projectId = UUID.randomUUID()
         val request = getExampleRequest().copy { id = projectId.toString() }
@@ -34,7 +33,7 @@ class ExportProjectTest : MainServiceTest() {
         mockCurrentUser(user)
         mockkObject(ProjectExportManager)
         every { ProjectExportManager.getSupportedFormats() } returns setOf(ExportFormat.JSON)
-        coEvery { projectMemberRepoMock.isProjectMember(projectId, user.id) } returns true
+        coJustRun { projectAccessCheckerMock.isAllowedToReadProject(user, projectId) }
         coEvery { projectRepoMock.getProjectById(projectId) } returns Result.success(DataBuilder.createExampleProject())
         coEvery { projectMemberRepoMock.getProjectMembersWithUsers(projectId) } returns emptyList()
         coEvery {
@@ -50,7 +49,7 @@ class ExportProjectTest : MainServiceTest() {
     }
 
     @Test
-    fun `When the non-project-member exports a project, then an UnauthorizedReadException is thrown`() = runTest {
+    fun `When the user exports a project, but has no access, then a TestSpecificException is thrown`() = runTest {
         val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
         val project = DataBuilder.createExampleProject()
         val request = getExampleRequest().copy { id = project.id.toString() }
@@ -58,51 +57,23 @@ class ExportProjectTest : MainServiceTest() {
         mockCurrentUser(user)
         mockkObject(ProjectExportManager)
         every { ProjectExportManager.getSupportedFormats() } returns setOf(ExportFormat.JSON)
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-        coEvery { projectMemberRepoMock.isProjectMember(project.id, user.id) } returns false
+        coEvery { projectAccessCheckerMock.isAllowedToReadProject(user, project.id) } throws TestSpecificException()
 
-        assertThrows<UnauthorizedReadException> {
-            mainService.exportProject(request)
-        }
+        assertThrows<TestSpecificException> { mainService.exportProject(request) }
     }
 
     @Test
-    fun `When a server admin export a project, then no exception is thrown`() = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
-        val projectId = UUID.randomUUID()
-        val request = getExampleRequest().copy { id = projectId.toString() }
+    fun `When retrieving the project fails, then a TestSpecificException is thrown`() = runTest {
+        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
+        val project = DataBuilder.createExampleProject()
+        val request = getExampleRequest().copy { id = project.id.toString() }
 
         mockCurrentUser(user)
         mockkObject(ProjectExportManager)
         every { ProjectExportManager.getSupportedFormats() } returns setOf(ExportFormat.JSON)
-        coEvery { projectMemberRepoMock.isProjectMember(projectId, user.id) } returns false
-        coEvery {
-            projectRepoMock.getProjectById(projectId)
-        } returns Result.success(DataBuilder.createExampleProject(id = projectId))
-        coEvery { projectMemberRepoMock.getProjectMembersWithUsers(projectId) } returns emptyList()
-        coEvery {
-            projectPaperRepoMock.getAllProjectPapersWithPapers(projectId)
-        } returns listOf(DataBuilder.createExampleProjectPaperWithPaper())
-        coEvery { reviewRepoMock.getAllReviewsWithSelectedCriteriaIdsForProjectPaper(any()) } returns emptyList()
-        coEvery { criterionRepoMock.getAllProjectCriteria(any()) } returns emptyList()
-        every {
-            ProjectExportManager.exportProject(any(), any(), any(), any(), any())
-        } returns FileExport(ByteArray(0), "test.json")
+        coJustRun { projectAccessCheckerMock.isAllowedToReadProject(user, project.id) }
+        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.failure(TestSpecificException())
 
-        mainService.exportProject(request)
-    }
-
-    @Test
-    fun `When the exported project doesn't exist, then a ProjectNotFoundException is thrown`() = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
-        val projectId = UUID.randomUUID()
-        val request = getExampleRequest().copy { id = projectId.toString() }
-
-        mockCurrentUser(user)
-        mockkObject(ProjectExportManager)
-        every { ProjectExportManager.getSupportedFormats() } returns setOf(ExportFormat.JSON)
-        coEvery { projectRepoMock.getProjectById(projectId) } returns Result.failure(TestSpecificException())
-
-        assertThrows<ProjectNotFoundException> { mainService.exportProject(request) }
+        assertThrows<TestSpecificException> { mainService.exportProject(request) }
     }
 }
