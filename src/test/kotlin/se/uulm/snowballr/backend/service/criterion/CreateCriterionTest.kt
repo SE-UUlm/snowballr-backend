d41b38926e114da325bea4dd655d0b0b57ce16a7
Felix Schlegel <felix.schlegel@gmx.de>
2026-05-01 20:59:24 +0200

    test: don't test every access path for the CriterionService

--- src/test/kotlin/se/uulm/snowballr/backend/service/criterion/CreateCriterionTest.kt
+++ src/test/kotlin/se/uulm/snowballr/backend/service/criterion/CreateCriterionTest.kt
@@ -1,18 +1,15 @@
 package se.uulm.snowballr.backend.service.criterion
 
 import io.mockk.coEvery
+import io.mockk.coJustRun
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import se.uulm.snowballr.backend.DataBuilder
-import se.uulm.snowballr.backend.model.exception.FailedPreconditionException
-import se.uulm.snowballr.backend.model.exception.UnauthorizedException
+import se.uulm.snowballr.backend.TestSpecificException
 import se.uulm.snowballr.backend.service.MainServiceTest
-import se.uulm.snowballr.backend.utils.GrpcEnumSourceTest
 import snowballr.CriterionOuterClass.CriterionCategory
-import snowballr.ProjectOuterClass.ProjectStatus
-import snowballr.UserOuterClass.UserRole
 import snowballr.CriterionOuterClass.Criterion as GrpcCriterion
 
 class CreateCriterionTest : MainServiceTest() {
@@ -36,76 +33,39 @@ class CreateCriterionTest : MainServiceTest() {
     }
 
     @Test
-    fun `When an admin user creates a project criterion, then no exception is thrown`() = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
-        val project = DataBuilder.createExampleProject()
-
-        val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id)
-        val request = getProjectCriterionRequest(project.id.toString())
-
-        mockCurrentUser(user)
-        coEvery { projectMemberRepoMock.getAllProjectAdmins(project.id) } returns emptyList()
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-        coEvery { criterionRepoMock.createCriterion(request, user.id) } returns criterion
-
-        assertDoesNotThrow { mainService.createCriterion(request) }
-    }
-
-    @Test
-    fun `When a project admin creates a project criterion, then no exception is thrown`() = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
-        val project = DataBuilder.createExampleProject()
-        val projectMember = DataBuilder.createExampleProjectMember(userId = user.id, projectId = project.id)
-
-        val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id)
-        val request = getProjectCriterionRequest(project.id.toString())
-
-        mockCurrentUser(user)
-        coEvery { projectMemberRepoMock.getAllProjectAdmins(project.id) } returns listOf(projectMember)
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-        coEvery { criterionRepoMock.createCriterion(request, user.id) } returns criterion
-
-        assertDoesNotThrow { mainService.createCriterion(request) }
-    }
-
-    @Test
-    fun `When a non project admin creates a project criterion, then an UnauthorizedException is thrown`() = runTest {
+    fun `When a user creates a project criterion and has access, then no exception is thrown`() = runTest {
         val user = DataBuilder.createExampleUser()
         val project = DataBuilder.createExampleProject()
 
+        val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id)
         val request = getProjectCriterionRequest(project.id.toString())
 
         mockCurrentUser(user)
-        coEvery { projectMemberRepoMock.getAllProjectAdmins(project.id) } returns emptyList()
+        coJustRun { criterionAccessCheckerMock.isAllowedToCreateProjectCriterion(user, project.id) }
+        coEvery { criterionRepoMock.createCriterion(request, user.id) } returns criterion
 
-        assertThrows<UnauthorizedException> { mainService.createCriterion(request) }
-    }
-
-    @GrpcEnumSourceTest(
-        ProjectStatus::class,
-        excludes = ["PROJECT_STATUS_ACTIVE", "PROJECT_STATUS_ACTIVE_LOCKED", "PROJECT_STATUS_UNSPECIFIED"],
-    )
-    fun `When a project admin creates a project criterion for a non active project, then a FailedPreconditionException is thrown`(
-        status: ProjectStatus,
-    ) = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
-        val project = DataBuilder.createExampleProject(
-            status = status,
-        )
-        val projectAdmin = DataBuilder.createExampleProjectMember(userId = user.id, projectId = project.id)
-
-        val request = getProjectCriterionRequest(project.id.toString())
-
-        mockCurrentUser(user)
-        coEvery { projectMemberRepoMock.getAllProjectAdmins(project.id) } returns listOf(projectAdmin)
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-
-        assertThrows<FailedPreconditionException> { mainService.createCriterion(request) }
+        assertDoesNotThrow { mainService.createCriterion(request) }
     }
 
     @Test
-    fun `When an user creates a user criterion, then no exception is thrown`() = runTest {
-        val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
+    fun `When a user creates a project criterion, but has no access, then a TestSpecificException is thrown`() =
+        runTest {
+            val user = DataBuilder.createExampleUser()
+            val project = DataBuilder.createExampleProject()
+
+            val request = getProjectCriterionRequest(project.id.toString())
+
+            mockCurrentUser(user)
+            coEvery {
+                criterionAccessCheckerMock.isAllowedToCreateProjectCriterion(user, project.id)
+            } throws TestSpecificException()
+
+            assertThrows<TestSpecificException> { mainService.createCriterion(request) }
+        }
+
+    @Test
+    fun `When an user creates a user criterion and has access, then no exception is thrown`() = runTest {
+        val user = DataBuilder.createExampleUser()
 
         val criterion = DataBuilder.createExampleUserCriterion()
         val request = getUserCriterionRequest()
--- src/test/kotlin/se/uulm/snowballr/backend/service/criterion/GetAllCriteriaForProjectTest.kt
+++ src/test/kotlin/se/uulm/snowballr/backend/service/criterion/GetAllCriteriaForProjectTest.kt
@@ -1,68 +1,38 @@
 package se.uulm.snowballr.backend.service.criterion
 
 import io.mockk.coEvery
+import io.mockk.coJustRun
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import se.uulm.snowballr.backend.DataBuilder
 import se.uulm.snowballr.backend.TestSpecificException
-import se.uulm.snowballr.backend.model.exception.UnauthorizedException
-import se.uulm.snowballr.backend.model.exception.notfound.entity.ProjectNotFoundException
 import se.uulm.snowballr.backend.service.MainServiceTest
-import snowballr.UserOuterClass.UserRole
 
 class GetAllCriteriaForProjectTest : MainServiceTest() {
     @Test
-    fun `When the project doesn't exist, then a ProjectNotFoundException is thrown`() = runTest {
-        val project = DataBuilder.createExampleProject()
-        val adminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
+    fun `When a user retrieves all criteria for a project, but has no access, then a TestSpecificException is thrown`() =
+        runTest {
+            val user = DataBuilder.createExampleUser()
+            val project = DataBuilder.createExampleProject()
 
-        mockCurrentUser(adminUser)
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.failure(TestSpecificException())
+            mockCurrentUser(user)
+            coEvery { projectAccessCheckerMock.isAllowedToReadProject(user, project.id) } throws TestSpecificException()
 
-        assertThrows<ProjectNotFoundException> { mainService.getAllCriteriaForProject(project.id) }
-    }
+            assertThrows<TestSpecificException> { mainService.getAllCriteriaForProject(project.id) }
+        }
 
     @Test
-    fun `When the requesting user is a server admin, then no exception is thrown`() = runTest {
-        val adminUser = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_ADMIN)
+    fun `When a user retrieves all criteria for a project and has access, then no exception is thrown`() = runTest {
+        val user = DataBuilder.createExampleUser()
         val project = DataBuilder.createExampleProject()
-        val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id, createdBy = adminUser.id)
+        val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id, createdBy = user.id)
 
-        mockCurrentUser(adminUser)
-        coEvery { projectMemberRepoMock.isProjectMember(project.id, adminUser.id) } returns false
-        coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
+        mockCurrentUser(user)
+        coJustRun { projectAccessCheckerMock.isAllowedToReadProject(user, project.id) }
         coEvery { criterionRepoMock.getAllProjectCriteria(project.id) } returns listOf(criterion)
 
         assertDoesNotThrow { mainService.getAllCriteriaForProject(project.id) }
     }
-
-    @Test
-    fun `When the requesting user is a project member and wants to retrieve all project criteria, then no exception is thrown`() =
-        runTest {
-            val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
-            val project = DataBuilder.createExampleProject()
-            val criterion = DataBuilder.createExampleProjectCriterion(projectId = project.id, createdBy = user.id)
-
-            mockCurrentUser(user)
-            coEvery { projectMemberRepoMock.isProjectMember(project.id, user.id) } returns true
-            coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-            coEvery { criterionRepoMock.getAllProjectCriteria(project.id) } returns listOf(criterion)
-
-            assertDoesNotThrow { mainService.getAllCriteriaForProject(project.id) }
-        }
-
-    @Test
-    fun `When the requesting user is a non project member and wants to retrieve all project criteria, then an UnauthorizedException is thrown`() =
-        runTest {
-            val user = DataBuilder.createExampleUser(role = UserRole.USER_ROLE_DEFAULT)
-            val project = DataBuilder.createExampleProject()
-
-            mockCurrentUser(user)
-            coEvery { projectRepoMock.getProjectById(project.id) } returns Result.success(project)
-            coEvery { projectMemberRepoMock.isProjectMember(project.id, user.id) } returns false
-
-            assertThrows<UnauthorizedException> { mainService.getAllCriteriaForProject(project.id) }
-        }
 }
