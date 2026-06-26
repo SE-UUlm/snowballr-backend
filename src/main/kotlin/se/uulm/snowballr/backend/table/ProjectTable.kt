0d7fbbf274afd2feda0fb9be4a8bbe8e46100436
Felix Schlegel <felix.schlegel@gmx.de>
2026-06-27 00:53:33 +0200

    chore: fix imports of comments

--- src/main/kotlin/se/uulm/snowballr/backend/table/ProjectTable.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/table/ProjectTable.kt
@@ -8,11 +8,11 @@ import org.jetbrains.exposed.v1.datetime.timestampWithTimeZone
 import se.uulm.snowballr.backend.model.dto.Project
 import se.uulm.snowballr.backend.model.dto.project.ProjectStatus
 import se.uulm.snowballr.backend.model.dto.project.SnowballingType
+import se.uulm.snowballr.backend.model.dto.projectpaper.PaperDecision
+import se.uulm.snowballr.backend.model.dto.review.ReviewDecision
 import se.uulm.snowballr.backend.model.fetcher.FetcherMap
 import se.uulm.snowballr.backend.table.columntypes.obfuscatedJson
-import snowballr.ProjectOuterClass.PaperDecision
 import snowballr.ProjectOuterClass.ReviewDecisionMatrix
-import snowballr.ReviewOuterClass.ReviewDecision
 import java.time.OffsetDateTime
 
 /**
