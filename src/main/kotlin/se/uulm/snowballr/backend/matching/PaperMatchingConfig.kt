0fa47bda98e49f30aa36643452445e1bb783e29f
Felix Schlegel <felix.schlegel@gmx.de>
2026-08-08 14:13:18 +0200

    feat: add more validation for PaperMatchingConfig

--- src/main/kotlin/se/uulm/snowballr/backend/matching/PaperMatchingConfig.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/matching/PaperMatchingConfig.kt
@@ -7,8 +7,11 @@ data class PaperMatchingConfig(
     val abstractWeight: Double,
 ) {
     init {
+        require(yearTolerance >= 0) { "The year tolerance must be positive or 0." }
+        require(titleWeight >= 0) { "titleWeight must be positive or 0" }
+        require(authorsWeight >= 0) { "authorsWeight must be positive or 0" }
+        require(abstractWeight >= 0) { "abstractWeight must be positive or 0" }
         val weightSum = titleWeight + authorsWeight + abstractWeight
         require(weightSum > 0.0) { "The sum of the weights must be positive." }
-        require(yearTolerance >= 0) { "The year tolerance must be positive or 0." }
     }
 }
