dbb425b658f0915cbb588ab841d0091fb3db3995
Felix Schlegel <felix.schlegel@gmx.de>
2026-09-03 19:59:01 +0200

    feat: add settings fields to UserField enum

--- src/main/kotlin/se/uulm/snowballr/backend/model/dto/user/UserField.kt
+++ src/main/kotlin/se/uulm/snowballr/backend/model/dto/user/UserField.kt
@@ -6,4 +6,36 @@ enum class UserField {
     LAST_NAME,
     ROLE,
     STATUS,
+
+    // Settings
+    ARE_HOTKEYS_SHOWN,
+    IS_REVIEW_MODE_ENABLED,
+    CRITERIA_IDS,
+    SIMILARITY_THRESHOLD,
+    SNOWBALLING_TYPE,
+    REVIEW_MAYBE_ALLOWED,
+    FETCHERS,
+    NUMBER_OF_REVIEWERS,
+    DECISION_MATRIX_PATTERNS,
+    ;
+
+    fun isDecisionMatrixField() = when (this) {
+        EMAIL,
+        FIRST_NAME,
+        LAST_NAME,
+        ROLE,
+        STATUS,
+        ARE_HOTKEYS_SHOWN,
+        IS_REVIEW_MODE_ENABLED,
+        CRITERIA_IDS,
+        SIMILARITY_THRESHOLD,
+        SNOWBALLING_TYPE,
+        REVIEW_MAYBE_ALLOWED,
+        FETCHERS,
+        -> false
+
+        NUMBER_OF_REVIEWERS,
+        DECISION_MATRIX_PATTERNS,
+        -> true
+    }
 }
